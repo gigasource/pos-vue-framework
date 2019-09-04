69b4a87240b0c07252967e3be48a5e0162bc60f0
longnh <hailong.nguyen95@gmail.com>
2019-09-04 17:32:19 +0700

    add text-field props

--- src/components/GInput/GTextField.vue
+++ src/components/GInput/GTextField.vue
@@ -5,10 +5,25 @@
 <script>
   export default {
     name: 'GTextField',
-    data()
-    {
-      return {}
-    }
+		props: {
+      label: String,
+			required: Boolean,
+			placeholder: String,
+			disabled: Boolean,
+			readonly: Boolean,
+			clearable: Boolean,
+			value: String
+		},
+		computed: {
+      computedValue: {
+        get() {
+          return this.value;
+				},
+				set (value) {
+          this.$emit('input', value);
+				}
+			}
+		}
   }
 </script>
 
