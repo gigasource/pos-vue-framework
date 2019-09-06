a955fc8aaebe72b5fa807457214af25d77a4c5d9
longnh <hailong.nguyen95@gmail.com>
2019-09-06 09:27:14 +0700

    add event handlers

--- src/components/GButton.vue
+++ src/components/GButton.vue
@@ -1,57 +1,69 @@
 <template>
-	<button :class="buttonClass" :style="buttonStyle">
-		<slot>Submit</slot>
+	<button :class="buttonClass" :style="buttonStyle" @click="onClick">
+		<slot>{{text}}</slot>
 	</button>
 </template>
 
 <script>
   export default {
     name: 'GButton',
-		props: {
+    props: {
       raised: Boolean,
-			depressed: Boolean,
-			disabled: Boolean,
-			flat: Boolean,
-			icon: Boolean,
-			outline: Boolean,
-			dashed: Boolean,
-			width: String,
-			height: String,
-			color: String,
-			textColor: String,
-		},
+      depressed: Boolean,
+      disabled: Boolean,
+      flat: Boolean,
+      icon: Boolean,
+      outline: Boolean,
+      dashed: Boolean,
+      width: String,
+      height: String,
+      color: String,
+      textColor: String,
+      text: String
+    },
     computed: {
       buttonClass() {
-				return {
-				  'btn-raised': this.raised,
-					'bs-0': this.depressed,
-					'disabled': this.disabled,
-					'br-100': this.icon,
-					'btn-outline': this.outline,
-					'b-dashed': this.dashed
-				}
+        return {
+          'btn-raised': this.raised,
+          'bs-0': this.depressed,
+          'disabled': this.disabled,
+          'br-100': this.icon,
+          'btn-outline': this.outline,
+          'b-dashed': this.dashed
+        }
       },
       buttonStyle() {
         let style = {};
-        if(this.height) {
-          Object.assign(style, {height: this.height});
-				}
-        if(this.width) {
-          Object.assign(style, {width: this.width});
-				}
-				if(this.outline){
-				  Object.assign(style, {'border-color': this.color, color: this.color});
-				} else if(this.color) {
-				  Object.assign(style, {'background-color': this.color});
-				}
-				if(this.flat) {
-          Object.assign(style, {'border-color': 'transparent', 'background-color': 'transparent'});
-				}
-				if(this.textColor){
-          Object.assign(style, {color: this.textColor});
-				}
+        if (this.height) {
+          Object.assign(style, { height: this.height });
+        }
+        if (this.width) {
+          Object.assign(style, { width: this.width });
+        }
+        if (this.outline) {
+          Object.assign(style, {
+            'border-color': this.color,
+            color: this.color
+          });
+        } else if (this.color) {
+          Object.assign(style, { 'background-color': this.color });
+        }
+        if (this.flat) {
+          Object.assign(style, {
+            'border-color': 'transparent',
+            'background-color': 'transparent'
+          });
+        }
+        if (this.textColor) {
+          Object.assign(style, { color: this.textColor });
+        }
         return style;
-			}
+      }
+    },
+    methods: {
+      onClick(event) {
+        this.$emit('click', event);
+      }
     }
   }
 </script>
--- src/components/GInput/GTextField.vue
+++ src/components/GInput/GTextField.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div @click="onClick" @mouseup="onMouseUp" @mousedown="onMouseDown">
 		<label class="fw-400 dgray-3 fs-small-2 mb-2">
 			<slot name="label">{{label}}</slot>
 		</label>
@@ -8,49 +8,98 @@
 						 class="textfield br-2 bg-lgray-5 pa-2 fs-small fw-700"
 						 v-model="computedValue"
 						 :placeholder="placeholder"
-						 @change="$emit('change', $event)"
-						 @input="$emit('input', $event)"
-						 @click="$emit('click', $event)"
-						 @focus="$emit('focus', $event)"
-						 @blur="$emit('blur', $event)"
-						 @keydown="$emit('keydown', $event)"
-						 @mouseup="$emit('mouseup', $event)"
-						 @mousedown="$emit('mousedown', $event)"
+						 ref="input"
+						 @input="onInput"
+						 @change="onChange"
+						 @focus="onFocus"
+						 @blur="onBlur"
+						 @keydown="onKeyDown"
 			/>
 			<span class="textfield-icon">
-				<slot name="append">
-					<img v-if="clearable" src="../../assets/delivery/cancel.svg">
-				</slot>
+				<slot name="append"></slot>
+				<img v-if="isDirty" src="../../assets/delivery/cancel.svg" @click="clearValue">
 			</span>
 		</div>
 	</div>
 </template>
 
 <script>
+  import { keyCodes } from '@/utils/helpers';
+
   export default {
     name: 'GTextField',
-		props: {
+    props: {
       label: String,
-			required: Boolean,
-			placeholder: String,
-			disabled: Boolean,
-			readonly: Boolean,
-			clearable: Boolean,
-			value: String
+      required: Boolean,
+      placeholder: String,
+      disabled: Boolean,
+      readonly: Boolean,
+      clearable: Boolean,
+      value: [String, Number]
+    },
+		data() {
+       return {
+         showClearBtn: false
+			 }
 		},
-		computed: {
+    computed: {
       computedValue: {
         get() {
           return this.value;
-				},
-				set (value) {
+        },
+        set(value) {
           this.$emit('input', value);
-				}
+        }
+      },
+			isDirty() {
+        return (this.computedValue && this.computedValue.toString().length > 0)
 			}
-		},
-		methods: {
-      //TODO event handler
-		}
+    },
+    methods: {
+      //TODO input-number
+      onInput(event) {
+
+			},
+      onChange(event) {
+        this.$emit('change', event);
+      },
+      onClick(event) {
+        this.$refs.input.focus();
+      },
+      onFocus(event) {
+        if (!this.$refs.input) {
+          return;
+        }
+        if (document.activeElement !== this.$refs.input) {
+          this.$refs.input.focus();
+        } else {
+          this.$emit('focus', event);
+        }
+      },
+      onBlur(event) {
+        this.$emit('blur', event);
+      },
+      onKeyDown(event) {
+        if (event.keyCode === keyCodes.enter && this.isDirty()) {
+          this.$emit('change', this.computedValue);
+        }
+        this.$emit('keydown', event)
+      },
+      onMouseDown(event) {
+        if (event.target !== this.$refs.input) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        this.$emit('mousedown', event)
+      },
+      onMouseUp(event) {
+        this.onFocus();
+        this.$emit('mousedown', event)
+      },
+      clearValue() {
+        this.computedValue = '';
+			}
+    }
   }
 </script>
 
