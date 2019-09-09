2a813dd662f7907a552345e183e85db013a02f6d
longnh <hailong.nguyen95@gmail.com>
2019-09-09 09:42:01 +0700

    add g-window

--- src/components/GWindow/GWindow.vue
+++ src/components/GWindow/GWindow.vue
@@ -1,12 +1,30 @@
 <template>
-	
+	<div>
+		<slot>
+			<template v-for="item in items">
+				<g-window-item v-show="isActiveWindow(item)">
+					<slot name="item">
+					</slot>
+				</g-window-item>
+			</template>
+		</slot>
+	</div>
 </template>
 
 <script>
+  import GWindowItem from '@/components/GWindow/GWindowItem';
+
   export default {
     name: 'GWindow',
-    data() {
-      return {}
+    components: { GWindowItem },
+    props: {
+      items: Array,
+      value: Number
+    },
+    methods: {
+      isActiveWindow(window) {
+				return this.items[this.value] === window;
+      }
     }
   }
 </script>
