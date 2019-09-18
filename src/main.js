9fc7402eb6fbddbe3b954f00fa121e1ed76bc6ee
longnh <hailong.nguyen95@gmail.com>
2019-09-18 17:38:24 +0700

    refactor import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import VueCompositionApi from '@vue/composition-api';
 import router from './plugin/router';
 import VueRouter from 'vue-router';
-import waves from './plugin/waves';
+
+require('./plugin/waves');
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
@@ -11,6 +12,5 @@ Vue.use(VueRouter);
 
 new Vue({
   router,
-  waves,
   render: h => h(App),
 }).$mount('#app')
