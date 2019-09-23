5389d238008890bce45cc516c456bc1e4e239b9d
chuoikhungbo <quanvantruong73@gmail.com>
2019-09-23 17:28:53 +0700

    add range + fix slider

--- src/components/GInput/GRange.vue
+++ src/components/GInput/GRange.vue
@@ -1,130 +1,128 @@
 <template>
-	<div class="slider" :class="{'slider-disabled': disabled}" @click="onSliderClick" >
-		<input type="range" :value="internalValue" readonly/>
+	<div class="slider" :class="{'slider-disabled': disabled}" @click="onSliderClick">
+		<input type="range" :value="internalValue[0]" readonly>
+		<input type="range" :value="internalValue[1]" readonly>
 		<div class="slider-track-container" ref="track">
 			<div class="slider-track" :style="trackStyle"></div>
 			<div class="slider-fill" :style="trackFillStyle"></div>
 		</div>
-		<!-- add tabIndex=0 to enable focus/blur for div element -->
-		<div class="slider-thumb-container" ref="thumb" :style="thumbContainerStyle" tabindex="0"
-				 @focus="onFocus" @blur="onBlur" @keyup="onKeyUp" @keydown="onKeyDown" @mousedown="onThumbMouseDown" @touchstart="onThumbMouseDown">
-			<div class="slider-thumb" :style="thumbStyle"></div>
-			<div v-show="showLabel" class="slider-thumb-label" :style="thumbLabelStyle">
-				<span>{{ parseInt(internalValue) }}</span>
+		<template v-for="(value, i) in internalValue">
+			<div class="slider-thumb-container" :ref="`thumb_${i}`" :style="thumbContainerStyle(i)" :tabindex="i"
+					 @focus="onFocus" @blur="onBlur" @keyup="onKeyUp(i)" @keydown="onKeyDown(i)" @mousedown="onThumbMouseDown" @touchstart="onThumbMouseDown">
+				<div class="slider-thumb" :style="thumbStyle"></div>
+				<div v-show="showLabel" class="slider-thumb-label" :style="thumbLabelStyle">
+					<span>{{ parseInt(value) }}</span>
+				</div>
 			</div>
-		</div>
-		<div v-if="tick" class="slider-tick-container">
-			<span class="slider-tick" v-for="(step, i) in steps" :key="i"></span>
-		</div>
+		</template>
 	</div>
 </template>
 
 <script>
-  import { addOnceEventListener, keyCodes, passiveSupported } from '../../utils/helpers';
+  import { addOnceEventListener, passiveSupported } from '../../utils/helpers';
   import { isEqual } from 'lodash';
 
   export default {
-    name: 'GSlider',
+    name: 'GRange',
 		props: {
       disabled: Boolean,
-			min: {
+      min: {
         type: [Number, String],
-				default: 0
-			},
-			max: {
-        type: [Number, String],
-				default: 100
-			},
-			color: {
-        type: String,
-				default: '#000000'
-			},
-			trackColor: {
-        type: String,
-				default: '#eeeeee'
-			},
-			trackFillColor: {
-        type: String,
-				default: '#000000'
-			},
-			thumbColor: {
-        type: String,
-				default: '#000000'
-			},
-			thumbLabel: [Boolean, String],
-			thumbLabelSize: {
-        type: [Number, String],
-				default: '2rem'
-			},
-			thumbLabelColor: String,
-			thumbLabelTextColor: {
-        type: String,
-				default: '#ffffff'
+        default: 0
       },
-			value: [Number, String],
-			vertical: Boolean,
-			step: [Number, String],
-			tick: Boolean,
-			tickLabels: Array,
-			tickSize: {
+      max: {
         type: [Number, String],
-				default: 2
-			}
+        default: 100
+      },
+      color: {
+        type: String,
+        default: '#000000'
+      },
+      trackColor: {
+        type: String,
+        default: '#eeeeee'
+      },
+      trackFillColor: {
+        type: String,
+        default: '#000000'
+      },
+      thumbColor: {
+        type: String,
+        default: '#000000'
+      },
+      thumbLabel: [Boolean, String],
+      thumbLabelSize: {
+        type: [Number, String],
+        default: '2rem'
+      },
+      thumbLabelColor: String,
+      thumbLabelTextColor: {
+        type: String,
+        default: '#ffffff'
+      },
+      value: {
+        type: Array,
+				default: () => [0, 0]
+			},
 		},
 		data() {
       return {
-        lazyValue: 0,
+        lazyValue: [0, 0],
 				keyPressed: 0,
-        isFocused: false,
+				activeThumbIndex: -1,
 				isActive: false,
+				isFocused: false,
 				noClick: false,
 			}
 		},
 		computed: {
       minValue() {
         return parseFloat(this.min);
-			},
-			maxValue() {
+      },
+      maxValue() {
         return parseFloat(this.max);
-			},
-			internalValue: {
-			  get() {
-			    if(this.lazyValue)
-			    return this.lazyValue;
-				},
-				set(value) {
-			    value = isNaN(value) ? this.minValue : value;
-			    if(value === this.lazyValue) return;
-			    this.lazyValue = value;
-					this.$emit('input', value);
-				}
-			},
-			inputWidth() {
-        return (this.internalValue - this.minValue) / (this.maxValue - this.minValue) * 100;
-			},
-			trackStyle() {
+      },
+      internalValue: {
+        get() {
+          if(this.lazyValue)
+            return this.lazyValue;
+        },
+        set(value) {
+          let val = value.map(v => Math.min(Math.max(v, this.minValue), this.maxValue));
+          if(val[0] > val[1] || val[1] < val[0]){
+            val = [val[1], val[0]];
+					}
+          this.lazyValue = val;
+          this.$emit('input', val);
+        }
+      },
+      inputWidth() {
+        return this.internalValue.map(v => (v - this.minValue) / (this.maxValue - this.minValue) * 100);
+      },
+      trackStyle() {
         const startDir = this.vertical ? 'top' : 'right';
         const endDir = this.vertical ? 'height' : 'width';
         const bg = 'background-color';
 
         const start = '0';
-        const end = this.disabled ? `calc(${100 - this.inputWidth}% - 0.5rem)` : `calc(${100 - this.inputWidth}%)`;
-        const color = this.disabled ? this.trackFillColor : this.trackColor;
+        const end = 'calc(100%)';
+        const color = this.trackColor;
+
         return {
           [startDir]: start,
           [endDir]: end,
-					[bg]: color,
+          [bg]: color,
         }
-			},
-			trackFillStyle() {
+      },
+      trackFillStyle() {
         const startDir = this.vertical ? 'bottom' : 'left';
         const endDir = this.vertical ? 'top' : 'right';
         const valueDir = this.vertical ? 'height' : 'width';
         const bg = 'background-color';
 
-        const start = '0';
+        const start = this.disabled ? `calc(${this.inputWidth[0]}% + 0.5rem)` : `${this.inputWidth[0]}%`;
         const end = 'auto';
-        const value = this.disabled ? `calc(${this.inputWidth}% - 0.5rem)` : `${this.inputWidth}%`;
+        const value = this.disabled ? `calc(${Math.abs(this.inputWidth[0] - this.inputWidth[1])}% - 1rem)` : `calc(${Math.abs(this.inputWidth[0] - this.inputWidth[1])}%)`;
         const color = this.trackFillColor;
 
         return {
@@ -133,53 +131,45 @@
           [valueDir]: value,
           [bg]: color,
         }
-			},
-			thumbContainerStyle() {
+      },
+      thumbStyle() {
+        return {
+          'background-color': this.thumbColor
+        }
+      },
+      thumbLabelStyle() {
+        return {
+          'width': this.thumbLabelSize,
+          'height': this.thumbLabelSize,
+          'background-color': this.thumbLabelColor ? this.thumbLabelColor : this.trackFillColor,
+					'color': this.thumbLabelTextColor
+        }
+      },
+      showLabel() {
+        let showLabelOnChange = false;
+        if (typeof this.thumbLabel === 'string') {
+          if(this.thumbLabel === 'always') {
+            return true;
+          } else if (this.thumbLabel === 'true' ) {
+            showLabelOnChange = true;
+          }
+        } else showLabelOnChange = this.thumbLabel;
+        return (this.isFocused || this.isActive) && showLabelOnChange;
+      },
+		},
+		created() {
+      this.lazyValue = this.value.map(v => Math.min(Math.max(v, this.minValue), this.maxValue));
+    },
+		methods: {
+      thumbContainerStyle(index) {
         const direction = this.vertical ? 'top' : 'left';
-        let value = this.inputWidth;
+        let value = this.inputWidth[index];
         value = this.vertical ? 100 - value : value;
 
         return {
           [direction]: `${value}%`,
         }
-			},
-			thumbStyle() {
-        return {
-          'background-color': this.thumbColor
-				}
-			},
-			thumbLabelStyle() {
-        return {
-          'width': this.thumbLabelSize,
-					'height': this.thumbLabelSize,
-					'background-color': this.thumbLabelColor ? this.thumbLabelColor : this.trackFillColor,
-					'color': this.thumbLabelTextColor
-				}
-			},
-			showLabel() {
-        let showLabelOnChange = false;
-        if (typeof this.thumbLabel === 'string') {
-          if(this.thumbLabel === 'always') {
-            return true;
-					} else if (this.thumbLabel === 'true' ) {
-            showLabelOnChange = true;
-					}
-				} else showLabelOnChange = this.thumbLabel;
-        return (this.isFocused || this.isActive) && showLabelOnChange;
-			},
-			steps() {
-				//TODO slider ticks + steps
-			}
-		},
-		created() {
-			this.lazyValue = typeof this.value === 'string' ? parseFloat(this.value) : this.value;
-			if(this.lazyValue > this.maxValue) {
-			  this.lazyValue = this.maxValue;
-			} else if(this.lazyValue < this.minValue){
-			  this.lazyValue = this.minValue;
-			}
-    },
-		methods: {
+      },
       onThumbMouseDown (e) {
         this.oldValue = this.internalValue;
         this.keyPressed = 2;
@@ -213,11 +203,17 @@
         this.isActive = false;
         if(this.isFocused) {
           this.isFocused = false;
-				}
+        }
+        this.activeThumbIndex = -1;
       },
       onMouseMove (e) {
-        const { value } = this.parseMouseMove(e);
-        this.internalValue = value;
+        const { value, isInsideTrack } = this.parseMouseMove(e)
+
+        if (isInsideTrack) {
+          this.activeThumbIndex = this.getIndexOfClosestValue(this.internalValue, value)
+        }
+
+        this.setInternalValue(value)
       },
       onKeyDown (e) {
         if (this.disabled || this.readonly) return;
@@ -233,23 +229,32 @@
         this.keyPressed = 0;
       },
       onSliderClick (e) {
-        if (this.noClick) {
-          this.noClick = false;
-          return;
+        if (!this.isActive) {
+          if (this.noClick) {
+            this.noClick = false;
+            return
+          }
+
+          const { value, isInsideTrack } = this.parseMouseMove(e);
+          if (isInsideTrack) {
+            this.activeThumbIndex = this.getIndexOfClosestValue(this.internalValue, value);
+            const refName = `thumb_${this.activeThumbIndex}`;
+            const thumbRef = this.$refs[refName][0]; //v-for generate ref to array
+            thumbRef.focus();
+          }
+
+          this.setInternalValue(value);
+
+          this.$emit('change', this.internalValue);
         }
-        const thumb = this.$refs.thumb;
-				thumb.focus();
-        this.onMouseMove(e);
-        this.$emit('change', this.internalValue);
       },
       onBlur (e) {
         this.isFocused = false;
-
+				this.activeThumbIndex = -1;
         this.$emit('blur', e);
       },
       onFocus (e) {
         this.isFocused = true;
-
         this.$emit('focus', e);
       },
       parseMouseMove (e) {
@@ -300,6 +305,20 @@
         }
 
         return value;
+      },
+      getIndexOfClosestValue (array, value) {
+        if (Math.abs(array[0] - value) < Math.abs(array[1] - value))
+          return 0;
+        else
+          return 1;
+      },
+      setInternalValue (value) {
+        this.internalValue = this.internalValue.map((v, i) => {
+          if (i === this.activeThumbIndex)
+            return value;
+          else
+            return Number(v);
+        })
       },
 		}
   }
