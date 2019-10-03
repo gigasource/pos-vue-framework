19d349cb2a122b9c4a3e73f19889a2bbda41e20b
Pham Anh Tuan <t@u.n>
2019-10-03 11:18:35 +0700

    Fix CSS: height adjustment, remove content div, add padding for child elements Add border radius for card

--- src/view/CardDemo.vue
+++ src/view/CardDemo.vue
@@ -48,7 +48,7 @@
 			</g-card-actions>
 		</g-card>
 
-		<g-card flat elevation="24">
+		<g-card flat elevation="24" border-radius="20px">
 			<g-card-title>
 				<p>GCard Flat/Elevation=24</p>
 			</g-card-title>
@@ -72,10 +72,18 @@
 				<p>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed
 					making
 					you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to
+					find a way to escape. I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed
+					making
+					you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to
+					find a way to escape. I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed
+					making
+					you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to
 					find a way to escape.</p>
 			</g-card-text>
 			<g-card-actions>
 				<g-button>DON'T CLICK!</g-button>
+				<g-button>CLICK ME!</g-button>
+				<g-button>CLICK ME!</g-button>
 				<g-button>CLICK ME!</g-button>
 			</g-card-actions>
 		</g-card>
