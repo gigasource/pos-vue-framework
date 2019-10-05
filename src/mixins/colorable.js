ff93f5451c1636e0d398fcae158a036c30f759f1
ThinhVu <vutrongthinhk7@gmail.com>
2019-10-05 16:41:48 +0700

    + Update colorable and colorable test

--- src/mixins/colorable.js
+++ src/mixins/colorable.js
@@ -4,34 +4,42 @@
  * @param color {String | any} color value
  * @returns {boolean} true if input color is CSS color. Otherwise, false
  */
-function isCssColor(color) {
+export /*public for test only*/ function isCssColor(color) {
   return !!color && !!color.match(/^(#|(rgb|hsl)a?\()/)
 }
 
 /**
+ * Return true if data should be changed
+ * @param color {any} color value
+ * @param data {Object | any}
+ * @returns {boolean}
+ */
+
+export /*public for test only*/ function shouldApplyColor(color, data) {
+  if (typeof color !== 'string') return false
+  if (data == null) return false
+  if (typeof data !== 'object') return false
+  if (Array.isArray(data)) return false
+  if (typeof data.style === 'string') return false
+  if (typeof data.class === 'string') return false
+  return true
+}
+
+/**
  * Adding color for data object
- * @param color {String} CSS color or color name
+ * @param color {String | any} CSS color or color name
  * @param data {object | undefined} plain object
  * @returns {*|{}} data
- *  - If data is undefined, return {}
  *  - If data.style is string or data.class is string, return data without any modification
  *  - If color is CSS color, add 'background-color' & 'border-color' with value is color
  *  - If color is not CSS color, add [color] as property in data.class
  */
-function setBackgroundColor(color, data) {
+export function setBackgroundColor(color, data) {
   data = data || {}
-  if (typeof data.style === 'string') {
-    // istanbul ignore next
-    console.log('style must be an object', this)
-    // istanbul ignore next
+
+  if (!shouldApplyColor(color, data))
     return data
-  }
-  if (typeof data.class === 'string') {
-    // istanbul ignore next
-    console.log('class must be an object', this)
-    // istanbul ignore next
-    return data
-  }
+
   if (isCssColor(color)) {
     data.style = {
       ...data.style,
@@ -52,27 +60,18 @@ function setBackgroundColor(color, data) {
  * @param color {String} CSS color or color name
  * @param data {Object | undefined} plain object
  * @returns {*|{}}
- *  - If data is undefined, return {}
  *  - If data.style is string or data.class is string, return data without any modification
  *  - If color is CSS color, add 'color' & 'caret-color' prop to data.style object with value is color value
  *  - If color is not CSS color (accepted color format is: "<colorName>" or "<colorName> <colorModifier>"):
  *    + <colorName>: add [<colorName>--text] property in data.class
  *    + <colorName> <colorModifier>: add [<colorName>--text] and [text--<colorModifier>] in data.class
  */
-function setTextColor(color, data) {
+export function setTextColor(color, data) {
   data = data || {}
-  if (typeof data.style === 'string') {
-    // istanbul ignore next
-    console.log('style must be an object', this)
-    // istanbul ignore next
+
+  if (!shouldApplyColor(color, data))
     return data
-  }
-  if (typeof data.class === 'string') {
-    // istanbul ignore next
-    console.log('class must be an object', this)
-    // istanbul ignore next
-    return data
-  }
+
   if (isCssColor(color)) {
     data.style = {
       ...data.style,
