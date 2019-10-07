b78c0b56e567d584aa9625c5655cbb3aa3ee4577
chuoikhungbo <quanvantruong73@gmail.com>
2019-10-07 13:53:01 +0700

    add g-switch

---
+++ src/components/GSwitch/GSwitch.vue
@@ -0,0 +1,99 @@
+<template>
+	<div class="g-switch-wrapper">
+		<label class="g-switch-container">
+			<input type="checkbox">
+			<span class="g-switch" :class="classes" :style="styles" @click="activate">
+				<span class="g-switch-track"></span>
+				<span class="g-switch-thumb"></span>
+			</span>
+			<slot name="label">
+				<span class="g-switch-label">{{label}}</span>
+			</slot>
+		</label>
+	</div>
+</template>
+
+<script>
+  import { computed, ref, watch } from '@vue/composition-api';
+  import colorHandler from '../../mixins/colorHandler';
+
+  export default {
+    name: 'GSwitch',
+    model: {
+      prop: 'inputValue'
+    },
+    props: {
+      label: String,
+      color: String,
+      required: Boolean,
+      disabled: Boolean,
+      readonly: Boolean,
+      inset: Boolean,
+      flat: Boolean,
+      inputValue: null,
+      value: null
+    },
+    setup(props, context) {
+      let isActive = ref(false);
+      const internalValue = computed(() => props.inputValue);
+      const isSelectedArray = Array.isArray(internalValue.value);
+      const value = computed(() => props.value ? props.value : (internalValue.value && !isSelectedArray ? internalValue.value : isActive.value));
+      if (internalValue.value && isSelectedArray) {
+        isActive.value = internalValue.value.some(v => v === value.value)
+      } else if (internalValue.value === true || internalValue.value === 'true' || (internalValue.value === value.value)) {
+        isActive.value = true;
+      }
+
+      const { getColorType, convertColorClass } = colorHandler(props.color);
+      const type = getColorType();
+      const colorClass = convertColorClass('backg');
+      const classes = computed(() => ({
+        readonly: props.readonly,
+        disabled: props.disabled,
+        [colorClass]: !!type && type === 'class',
+        'g-switch__active': isActive.value,
+        'g-switch__flat': props.flat,
+        'g-switch__inset': props.inset
+      }));
+
+      const styles = computed(() => {
+        const styles = {};
+        if (type === 'style') {
+          Object.assign(styles, { 'color': props.color });
+        }
+        return styles;
+      });
+
+      function activate() {
+        isActive.value = !isActive.value;
+        if (isSelectedArray) {
+          const arrValue = internalValue.value;
+          const index = arrValue.findIndex(v => v === value.value);
+          if (isActive.value && index === -1) {
+            arrValue.push(value.value);
+          } else if (!isActive.value && index > -1) {
+            arrValue.splice(index, 1);
+          }
+          context.emit('input', arrValue);
+        } else {
+          if (isActive.value) {
+            context.emit('input', value.value);
+          } else {
+            context.emit('input', null);
+          }
+        }
+      }
+
+      return {
+        classes,
+        styles,
+        isActive,
+        activate
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
