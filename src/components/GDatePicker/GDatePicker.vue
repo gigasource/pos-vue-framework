e71c07dc1a5c1819559feb742b981d468770314e
ThinhVu <vutrongthinhk7@gmail.com>
2019-10-08 11:58:50 +0700

    + GDatePicker initial

---
+++ src/components/GDatePicker/GDatePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Initial Date Picker
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GDatePicker',
+    props: {},
+    data: function () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/view/DatePickerDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <g-date-picker></g-date-picker>
+  </div>
+</template>
+<script>
+  import GDatePicker from '../components/GDatePicker/GDatePicker';
+
+  export default {
+    name: 'DatePickerDemo',
+    components: { GDatePicker },
+    props: {},
+    data: function () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
