8ac45bb736700a942cca97c2ef58dad7d7499d30
Pham Anh Tuan <t@u.n>
2019-10-10 15:23:06 +0700

    Normal chips without colors TODO: Fixing prepend icons TODO: Color settings

--- src/components/GChip/GChip.vue
+++ src/components/GChip/GChip.vue
@@ -1,54 +1,161 @@
 <template>
-	<div :class="classes" :style="styles">
-		<slot name="default">{{text}}</slot>
+	<div :class="classes" :style="styles" :draggable="attrs.draggable" @click="onClick">
+		<span class="g-icon g-icon__left">
+			<slot name="prependItem">
+				<i class="material-icons g-icon"  v-if="filter && stateData.isActive">{{filterIcon}}</i>
+			</slot>
+		</span>
+
+		<slot></slot>
+
+		<span class="g-icon g-icon__right" @click.stop="onClose">
+			<slot name="close" v-if="close">
+				<i class="material-icons g-icon">{{closeIcon}}</i>
+			</slot>
+		</span>
 	</div>
 </template>
 
 <script>
+  import { computed, reactive } from '@vue/composition-api';
+  import { convertToGradient } from '../../utils/helpers';
+  import { setBackgroundColor } from '../../mixins/colorable';
+
   export default {
     name: 'GChip',
     props: {
       active: { type: Boolean, default: true },
       append: Boolean,
       close: Boolean,
-      closeIcon: { type: String, default: `$delete` },
-      color: String,
-      disabled: Boolean,
-      draggable: Boolean,
-      filter: Boolean,
-      filterIcon: { type: String, default: `$complete` },
+      closeIcon: { type: String, default: `remove_circle` },
+      color: String,				//done
+      disabled: Boolean,   //done
+      filter: Boolean,		//done
+      filterIcon: { type: String, default: `done` },	//done //icons import?
       href: [String, Object],
       inputValue: null,
       label: Boolean,
-      large: Boolean,
       link: Boolean,
-      outlined: Boolean,
+      outlined: Boolean,  //re-style border color
       pill: Boolean,
       replace: Boolean,
-      ripple: Boolean,
-      small: Boolean,
+      ripple: Boolean,					//done
       target: String,
-      textColor: String,
+      textColor: String,				//done
+      backgroundColor: String,	//done
+      gradient: String,			//done
       value: null,
-      xLarge: Boolean,
-      xSmall: Boolean
+      //style
+      draggable: Boolean, //done
+      small: Boolean, //done
+      large: Boolean, //done
+      xSmall: Boolean, //done
+      xLarge: Boolean //done
     },
     setup(props, context) {
+      let stateData = reactive({
+        isActive: false
+      });
+
+      let toggle = () => (stateData.isActive = !stateData.isActive);
+
+      let colorOutput = computed(() => {
+        return setBackgroundColor(props.color, {})
+      });
+
+      const chipClasses = computed(() => {
+        return colorOutput.value.class
+      });
+
+      console.log(chipClasses)
+
       let classes = computed(() => {
+
         let _classes = {
           'g-chip': true,
-				};
+          'waves-effect': props.ripple,
+          'g-chip__outlined': props.outlined,
+          'g-chip__close': props.close,
+          'g-chip__disabled': props.disabled,
+          'g-chip__filter': props.filter,
+          'g-chip__draggable': props.draggable,
+          'g-chip__pill': props.pill,
+          'g-chip__label': props.label,
+          'g-chip__active': props.active,
+        };
+
+        let size = '';
+        //let iconSize = '';
+
+        if (props.large) {
+          size = 'g-size__large';
+          // iconSize = props.fab ? 'g-icon-size__large' : null;
+        } else if (props.small) {
+          size = 'g-size__small';
+          // iconSize = props.fab ? 'g-icon-size__small' : null;
+        } else if (props.xSmall) {
+          size = 'g-size__x-small';
+          // iconSize = props.fab ? 'g-icon-size__x-small' : null;
+        } else if (props.xLarge) {
+          size = 'g-size__x-large';
+          // iconSize = props.fab ? 'g-icon-size__x-large' : null;
+        } else {
+          size = 'g-size__default';
+          // iconSize = props.fab ? 'g-icon-size__default' : null;
+        }
+        if (size) {
+          _classes[size] = true;
+        }
+
+        // if (iconSize) {
+        //   _classes[iconSize] = true;
+        // }
+
+
         return _classes;
       });
 
       let styles = computed(() => {
-        let _styles = {};
+        let _styles = {
+          ...colorOutput.value.style,
+          ...props.textColor && { color: props.textColor.replace('-', '') },
+          ...props.backgroundColor && { backgroundColor: props.backgroundColor.replace('-', '') },
+          //...props.color && { backgroundColor: props.color.replace('-', ''), color: '#fff' },
+          //...props.outlined && { color: `${props.color}`, border: `thin solid currentColor`, backgroundColor: 'transparent' },
+        };
+
+        // Params: linear-gradient(45deg, yellow, green)
+        if (props.gradient && !props.gradient.toString().includes('-')) {
+          _styles['background-image'] = convertToGradient(props.gradient.toString().split(','), props.gradientAngle);
+        }
+
         return _styles;
       });
 
+      let attrs = computed(() => {
+        let _attrs = {
+          ...props.draggable && { draggable: props.draggable }
+        };
+        return _attrs;
+      });
+
+      let onClick = (event) => {
+        context.emit('click', event);
+        toggle();
+      };
+
+      let onClose = (event) => {
+        context.emit('click:close');
+        context.emit('update:active', false);
+      };
+
       return {
         classes,
-        styles
+        styles,
+        attrs,
+        stateData,
+        onClick,
+        onClose
       }
     }
   }
--- src/view/ChipDemo.vue
+++ src/view/ChipDemo.vue
@@ -1,20 +1,154 @@
 <template>
-<div class="wrapper">
-	<g-chip>TUAN</g-chip>
-</div>
+	<div class="container">
+		<div class="wrapper">
+			<p>Different sizes / default chips</p>
+			<hr/>
+			<g-chip x-small draggable close active filter>X Small</g-chip>
+			<g-chip small draggable filter filter-icon="check_circle">Small</g-chip>
+			<g-chip draggable filter filter-icon="check_circle">Default</g-chip>
+			<g-chip large draggable close filter filter-icon="check_circle">Large</g-chip>
+			<g-chip x-large draggable close filter filter-icon="check_circle">X Large</g-chip>
+		</div>
+		<div class="wrapper">
+			<p>Disabled chips</p>
+			<hr/>
+			<g-chip disabled>Disable</g-chip>
+			<g-chip disabled small color="blue">Blue</g-chip>
+			<g-chip disabled color="green">Green</g-chip>
+			<g-chip disabled large color="red">Red</g-chip>
+			<g-chip disabled large color="brown">Brown</g-chip>
+		</div>
+		<div class="wrapper">
+			<p>Label chips: Border radius of g-cards</p>
+			<hr/>
+			<g-chip x-small label close active ripple color="light-blue">Label</g-chip>
+			<g-chip small label close ripple>Label</g-chip>
+			<g-chip label close ripple>Label</g-chip>
+			<g-chip large label close ripple>Label</g-chip>
+			<g-chip x-large label close ripple>Label</g-chip>
+		</div>
+
+
+		<div class="wrapper">
+			<p>Outlined chips:Border is outlined</p>
+			<hr/>
+			<g-chip x-small outlined label close active color="green">Outlined</g-chip>
+			<g-chip small outlined label close>Outlined</g-chip>
+			<g-chip label outlined close>Outlined</g-chip>
+			<g-chip large outlined label close>Outlined</g-chip>
+			<g-chip x-large outlined label close>Outlined</g-chip>
+		</div>
+
+		<div class="wrapper">
+			<p>Avatar & Icons</p>
+			<hr/>
+			<!--			<g-chip>-->
+			<!--				<template v-slot:default>-->
+			<!--					<g-menu v-model="showMenu2" lazy open-on-hover open-delay="500" close-delay="500">-->
+			<!--						<template v-slot:activator="{toggleContent}">-->
+			<!--							<g-btn @click="toggleContent">Activator</g-btn>-->
+			<!--						</template>-->
+			<!--						<g-list>-->
+			<!--							<g-list-item> v-for="i in 5" :key="i">-->
+			<!--								<g-btn> {{i}}</g-btn>-->
+			<!--							</g-list-item>-->
+			<!--						</g-list>-->
+			<!--					</g-menu>-->
+			<!--				</template>-->
+			<!--				Close-->
+			<!--			</g-chip>-->
+			<g-chip>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Avatar
+			</g-chip>
+			<g-chip large ripple>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Avatar
+			</g-chip>
+			<g-chip x-large ripple>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Avatar
+			</g-chip>
+		</div>
+		<div class="wrapper">
+			<p>Pill</p>
+			<hr/>
+			<g-chip pill>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Pill
+			</g-chip>
+			<g-chip pill>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Pill
+			</g-chip>
+			<g-chip large pill>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Pill
+			</g-chip>
+			<g-chip x-large ripple pill>
+				<img class="g-avatar g-avatar__left avatar-image" src="https://loremflickr.com/320/240" alt="Avatar">
+				Pill
+			</g-chip>
+		</div>
+
+		<div class="wrapper">
+			<p>Icons</p>
+			<hr/>
+			<g-chip>
+				<span class="g-icon g-icon__left">
+					<i class="material-icons g-icon">account_circle</i>
+				</span>
+				Ranee
+			</g-chip>
+			<g-chip>
+				Premium
+				<span class="g-icon g-icon__right">
+					<i class="material-icons g-icon">star</i>
+				</span>
+			</g-chip>
+			<g-chip>
+				1 Year
+				<span class="g-icon g-icon__right">
+					<i class="material-icons g-icon">cake</i>
+				</span>
+			</g-chip>
+			<g-chip>
+				<span class="g-icon g-icon__left">
+				<i class="material-icons g-icon">check_circle</i>
+				</span>
+				Confirmed
+			</g-chip>
+			<g-chip close close-icon="delete">
+				<span class="g-icon g-icon__left">
+					<i class="material-icons g-icon">check_circle</i>
+				</span>
+				Confirmed
+			</g-chip>
+		</div>
+
+	</div>
 </template>
 
 <script>
   import GChip from '../components/GChip/GChip';
+  import GMenu from '../components/GMenu/GMenu';
+  import GBtn from '../components/GBtn/GBtn';
+  import GList from '../components/GList/GList';
+  import GListItem from '../components/GList/GListItem';
+
   export default {
     name: 'ChipDemo',
-    components: { GChip }
+    components: { GListItem, GList, GBtn, GMenu, GChip }
   }
 </script>
 
 <style scoped>
+	.avatar-image {
+		border-radius: 50%;
+	}
+
 	.wrapper {
-		display: flex;
+		display: block;
 		flex-wrap: wrap;
 	}
 
