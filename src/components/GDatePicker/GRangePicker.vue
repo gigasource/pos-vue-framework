0abcf63a0a27db1def00c2f28f1ff4479054eb18
ThinhVu <vutrongthinhk7@gmail.com>
2019-10-15 18:14:11 +0700

    + Add GDatePickerTitle + Set min/max for from/to

--- src/components/GDatePicker/GRangePicker.vue
+++ src/components/GDatePicker/GRangePicker.vue
@@ -1,23 +1,42 @@
 <template>
-  <div style="display: flex">
-    <g-date-picker no-title range :value="state.range" :type="'date'" v-on="startRangeEventHandler"></g-date-picker>
-    <g-date-picker no-title range :value="state.range" :type="'date'" v-on="endRangeEventHandler"></g-date-picker>
+  <div>
+    <g-date-picker-title
+        :date="selectedRanges"
+        :selecting-year="true"
+    />
+    <div style="display: flex">
+      <g-date-picker
+          no-title
+          range
+          :value="state.range"
+          :type="'date'"
+          :max="state.range[1]"
+          v-on="startRangeEventHandler"/>
+      <g-date-picker
+          no-title
+          range
+          :value="state.range"
+          :min="state.range[0]"
+          :type="'date'"
+          v-on="endRangeEventHandler"/>
+    </div>
   </div>
 </template>
 
 <script>
   import GDatePicker from './GDatePicker'
   import { EVENT_NAMES } from './GDatePickerUtil';
-  import { reactive } from '@vue/composition-api';
+  import { reactive, computed } from '@vue/composition-api';
+  import GDatePickerTitle from './Title/GDatePickerTitle';
 
 
   export default {
     name: 'GRangePicker',
-    components: { GDatePicker },
+    components: { GDatePickerTitle, GDatePicker },
     props: {},
     setup() {
       const state = reactive({
-        range: ['2019-01-01', '2019-01-02'],
+        range: ['2019-01-10', '2019-01-20'],
       })
 
       const startRangeEventHandler = {
@@ -32,8 +51,17 @@
         }
       }
 
+      const TICKS_PER_DAY = 864e5
+      const descOrder = (a, b) => a > b ? 1 : -1
+
+      const selectedRanges = computed(() => {
+        const [rangeFrom, rangeTo] = [state.range[0], state.range[1]].map(x => new Date(`${x}T00:00:00+00:00`)).sort(descOrder)
+        return (Math.ceil((rangeTo.getTime() - rangeFrom.getTime()) / TICKS_PER_DAY) + 1) + ' selected'
+      })
+
       return {
         state,
+        selectedRanges,
         startRangeEventHandler,
         endRangeEventHandler
       }
