0fb6ab957015578b7851d3a9f717a3cdb67c9ab6
SonlamBKIC <phamsonlam1996@gmail.com>
2019-10-24 15:16:50 +0700

    Refactor code

--- src/components/GDndDialog/GDndDialog3.vue
+++ src/components/GDndDialog/GDndDialog3.vue
@@ -1,6 +1,6 @@
 <template>
 	<transition name="dialog-transition">
-		<div v-if="isRender" v-show="isActive" class="g-dnddialog3" :class="dialogClasses" :style="dialogStyles" ref="dialog" @mousedown="dragStart">
+		<div v-if="isRender" v-show="isActive" class="g-dnddialog3-wrapper" :class="wrapperClasses" :style="wrapperStyles" ref="wrapper" @mousedown="dragStart">
 			<div class="g-dnddialog3-header" ref="header">
 				<span class="g-dnddialog3-title" ref="title">
 					<slot name="title"></slot>
@@ -24,9 +24,9 @@
 	</transition>
 </template>
 <script>
-	import { getTransitionDuration } from '../../utils/helpers';
-	import detachable from '../../mixins/detachable';
-	import getVModel from '../../mixins/getVModel';
+  import { getTransitionDuration } from '../../utils/helpers';
+  import detachable from '../../mixins/detachable';
+  import getVModel from '../../mixins/getVModel';
   import { computed, ref, reactive, watch, onMounted, onBeforeUnmount } from '@vue/composition-api';
   import GBtn from '../GBtn/GBtn';
 
@@ -39,111 +39,70 @@
         default: false
       },
 
-			// Render Options
-			lazy: Boolean,
-			destroyOnClose: Boolean,
+      // Render Options
+      lazy: Boolean,
+      destroyOnClose: Boolean,
 
-			minWidth: {
-			  type: [Number, String],
-				default: 200
-			},
+      // Start styles
+      minWidth: {
+        type: [Number, String],
+        default: 200
+      },
       minHeight: {
         type: [Number, String],
         default: 100
       },
-			width: [Number, String],
-			height: [Number, String]
-		},
-		setup (props, context) {
+      width: [Number, String],
+      height: [Number, String]
+    },
+    setup(props, context) {
       const { model: isActive } = getVModel(props, context)
       const { attachToRoot, detach } = detachable(props, context);
-      const isMinimize = ref(false)
-			const isMaximize = ref(false)
-      const isDrag = ref(false)
-      const isResize = ref(false)
-			const resizeMode = ref('')
-			const cursor = ref('')
-			const resizeRegionSize = 8;
 
-      const dialogPosition = reactive({
-        top: 0,
-        left: 0,
-				bottom: 0,
-				right: 0,
-      })
+      // Render Options Handling
+      const isRender = ref(false)
 
-      const dialogDimension = reactive({
-        width: 0,
-        height: 0
-      })
-
-			const minDialogDimension = {
-				width: +props.minWidth,
-				height: +props.minHeight,
-			}
-
-			const dialogStartPosition = {
-				top: 0,
-				left: 0,
-			}
-
-			const dialogStartDimension = {
-        width: 0,
-        height: 0
-			}
-
-			const mouseStartPosition = {
-				pageX: 0,
-				pageY: 0
-			}
-
-			watch([() => dialogPosition, () => dialogDimension], () => {
-			  dialogPosition.bottom = dialogPosition.top + dialogDimension.height;
-        dialogPosition.right = dialogPosition.left + dialogDimension.width;
-			}, { deep: true })
-
-
-			// Render Options Handling
-			const isRender = ref(false)
-
-			watch(isActive, (newVal) => {
-			  if (props.destroyOnClose) {
-			    if (newVal) {
+      watch(isActive, (newVal) => {
+        if (props.destroyOnClose) {
+          if (newVal) {
             isRender.value = isActive.value;
-					} else {
+          } else {
             setTimeout(() => {
               isRender.value = isActive.value;
-            }, getTransitionDuration(context.refs.dialog))
+            }, getTransitionDuration(context.refs.wrapper))
           }
-				}
+        }
 
-				if (props.lazy) {
-			  	if (newVal) {
+        if (props.lazy) {
+          if (newVal) {
             isRender.value = true;
             context.root.$nextTick(() => {
-              attachToRoot(context.refs.dialog);
+              attachToRoot(context.refs.wrapper);
             })
           }
-				}
-			}, {lazy: true})
+        }
+      }, { lazy: true })
 
-			onMounted(() => {
-				dialogDimension.width = props.width ? Math.max(+props.width, minDialogDimension.width) : minDialogDimension.width;
-				dialogDimension.height = props.height ? Math.max(+props.height, minDialogDimension.height) :minDialogDimension.height;
-				dialogPosition.top = (window.innerHeight - dialogDimension.height) / 2;
-				dialogPosition.left = (window.innerWidth - dialogDimension.width) / 2;
+      onMounted(() => {
         if (props.lazy) {
           return;
         }
-				attachToRoot(context.refs.dialog);
-			})
+        attachToRoot(context.refs.wrapper);
+      })
 
-			const dialogClasses = computed(() => ({
-				'g-dnddialog3__minimize': isMinimize.value,
-				'g-dnddialog3__maximize': isMaximize.value
-			}))
+      onBeforeUnmount(() => {
+        if (isRender.value) {
+          detach(context.refs.wrapper);
+        }
+      })
 
-      const dialogStyles = computed(() => ({
+      // Dynamic classes and styles
+      const wrapperClasses = computed(() => ({
+        'g-dnddialog3__minimize': isMinimize.value,
+        'g-dnddialog3__maximize': isMaximize.value
+      }))
+
+      const wrapperStyles = computed(() => ({
         top: isMaximize.value ? undefined : dialogPosition.top + 'px',
         left: isMaximize.value ? undefined : dialogPosition.left + 'px',
         width: isMaximize.value ? undefined : dialogDimension.width + 'px',
@@ -153,24 +112,80 @@
         cursor: cursor.value
       }))
 
+      // Windows actions
+      const isMinimize = ref(false)
+      const isMaximize = ref(false)
+
       function toggleDialog() {
         isActive.value = !isActive.value;
       }
 
       function toggleMinimize() {
         isMinimize.value = true;
-			}
+      }
 
-			function toggleMaximize() {
-        if (isMinimize.value) isMinimize.value = false
-				else {
-				  isMaximize.value = !isMaximize.value
-				}
-			}
+      function toggleMaximize() {
+        if (isMinimize.value) {
+          isMinimize.value = false
+        } else {
+          isMaximize.value = !isMaximize.value
+        }
+      }
 
-			function dragStart(e) {
+      // Dialog Positions and Dimensions
+      const dialogPosition = reactive({
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      })
+
+      const dialogDimension = reactive({
+        width: 0,
+        height: 0
+      })
+
+      const minDialogDimension = {
+        width: +props.minWidth,
+        height: +props.minHeight,
+      }
+
+      const dialogStartPosition = {
+        top: 0,
+        left: 0,
+      }
+
+      const dialogStartDimension = {
+        width: 0,
+        height: 0
+      }
+
+      const mouseStartPosition = {
+        pageX: 0,
+        pageY: 0
+      }
+
+      watch([() => dialogPosition, () => dialogDimension], () => {
+        dialogPosition.bottom = dialogPosition.top + dialogDimension.height;
+        dialogPosition.right = dialogPosition.left + dialogDimension.width;
+      }, { deep: true })
+
+      // Update start position and dimension on mounted
+      onMounted(() => {
+        dialogDimension.width = props.width ? Math.max(+props.width, minDialogDimension.width) : minDialogDimension.width;
+        dialogDimension.height = props.height ? Math.max(+props.height, minDialogDimension.height) : minDialogDimension.height;
+        dialogPosition.top = (window.innerHeight - dialogDimension.height) / 2;
+        dialogPosition.left = (window.innerWidth - dialogDimension.width) / 2;
+      })
+
+      //  Drag handlers
+      const isDrag = ref(false)
+
+      function dragStart(e) {
         e.preventDefault();
-        if (isMaximize.value) return;
+        if (isMaximize.value) {
+          return;
+        }
 
         const target = e.target;
         if ((target === context.refs.header || target === context.refs.title) && resizeMode.value === '') {
@@ -180,21 +195,21 @@
           mouseStartPosition.pageY = e.pageY;
           isDrag.value = true;
           cursor.value = 'move';
-				}
-			}
+        }
+      }
 
-			function drag(e) {
+      function drag(e) {
         e.preventDefault()
 
-				if (isDrag.value) {
-				  const newTop = dialogStartPosition.top - mouseStartPosition.pageY + e.pageY;
-				  const newLeft = dialogStartPosition.left - mouseStartPosition.pageX + e.pageX;
+        if (isDrag.value) {
+          const newTop = dialogStartPosition.top - mouseStartPosition.pageY + e.pageY;
+          const newLeft = dialogStartPosition.left - mouseStartPosition.pageX + e.pageX;
 
-				  if (newTop < 0) {
-				    dialogPosition.top = 0;
-					} else {
-				    dialogPosition.top = newTop;
-					}
+          if (newTop < 0) {
+            dialogPosition.top = 0;
+          } else {
+            dialogPosition.top = newTop;
+          }
 
           if (newLeft < 0) {
             dialogPosition.left = 0;
@@ -202,30 +217,51 @@
             dialogPosition.left = newLeft;
           }
         }
-			}
+      }
 
-			function dragEnd(e) {
+      function dragEnd(e) {
         e.preventDefault()
 
-        if(isDrag.value) {
+        if (isDrag.value) {
           isDrag.value = false;
           cursor.value = '';
-				}
-			}
+        }
+      }
 
-			function resizeStart(e) {
+      document.addEventListener('mousemove', drag);
+      document.addEventListener('mouseup', dragEnd);
+
+      onBeforeUnmount(() => {
+        document.removeEventListener('mousemove', drag);
+        document.removeEventListener('mouseup', dragEnd);
+      })
+
+      // Resize handlers
+      const isResize = ref(false)
+
+      /** Resize Mode : 8 modes
+       * n: North | s: South | e: East | w: West
+       * ne: North-East | nw: North-West | se: South-East | sw: South-West
+       */
+
+      const resizeMode = ref('')
+      const cursor = ref('')
+      const resizeRegionSize = 8;
+
+      function resizeStart(e) {
         e.preventDefault()
-				if (resizeMode.value !== '') {
+        if (resizeMode.value !== '') {
           dialogStartPosition.top = dialogPosition.top;
           dialogStartPosition.left = dialogPosition.left;
           dialogStartDimension.width = dialogDimension.width;
           dialogStartDimension.height = dialogDimension.height;
           mouseStartPosition.pageX = e.pageX;
           mouseStartPosition.pageY = e.pageY;
-				  isResize.value = true;
-				}
-			}
+          isResize.value = true;
+        }
+      }
 
+      // Methods for handling single resize direction (n,s,e,w)
       const resizeMethods = {
         n: function (e) {
           const newHeight = dialogStartDimension.height + mouseStartPosition.pageY - e.pageY
@@ -246,7 +282,7 @@
         w: function (e) {
           const newWidth = dialogStartDimension.width + mouseStartPosition.pageX - e.pageX
           const newLeft = dialogStartPosition.left - mouseStartPosition.pageX + e.pageX
-          if (newWidth >= minDialogDimension.width  && newLeft >= 0) {
+          if (newWidth >= minDialogDimension.width && newLeft >= 0) {
             dialogDimension.width = newWidth;
             dialogPosition.left = newLeft;
           }
@@ -260,96 +296,98 @@
         }
       }
 
-			function resize(e) {
+      function resize(e) {
         e.preventDefault()
-        if(isDrag.value || isMinimize.value || isMaximize.value) return;
+        if (isDrag.value || isMinimize.value || isMaximize.value) {
+          return;
+        }
 
         if (isResize.value) {
-					const resizeDirections = resizeMode.value.split('');
-					for (let resizeDirection of resizeDirections) {
-					  resizeMethods[resizeDirection](e);
-					}
-				} else {
+          // Split resize mode into separated resize direction then handle individual resize direction
+          const resizeDirections = resizeMode.value.split('');
+          for (let resizeDirection of resizeDirections) {
+            resizeMethods[resizeDirection](e);
+          }
+        } else {
+          // Set resize mode for each edge region of the dialog
           let tempResizeMode = '';
 
-          if (e.pageY >= dialogPosition.top - resizeRegionSize && e.pageY <= dialogPosition.top + resizeRegionSize)
-						tempResizeMode = 'n';
-					else if (e.pageY >= dialogPosition.bottom - resizeRegionSize && e.pageY <= dialogPosition.bottom + resizeRegionSize)
-						tempResizeMode ='s';
-					if (e.pageX >= dialogPosition.left - resizeRegionSize && e.pageX <= dialogPosition.left + resizeRegionSize)
-						tempResizeMode += 'w';
-					else if (e.pageX >= dialogPosition.right - resizeRegionSize && e.pageX <= dialogPosition.right + resizeRegionSize)
-						tempResizeMode += 'e';
-
-					resizeMode.value = tempResizeMode;
-
-          switch (tempResizeMode) {
-						case 'n':
-						case 's':
-						  cursor.value = 'ns-resize';
-						  document.body.style.cursor = 'ns-resize';
-						  break;
-						case 'e':
-						case 'w':
-						  cursor.value = 'ew-resize';
-              document.body.style.cursor = 'ew-resize';
-						  break;
-						case 'ne':
-						case 'sw':
-						  cursor.value = 'nesw-resize';
-              document.body.style.cursor = 'nesw-resize';
-							break;
-						case 'nw':
-						case 'se':
-						  cursor.value = 'nwse-resize';
-              document.body.style.cursor = 'nwse-resize';
-						  break;
-						default:
-						  cursor.value = '';
-              document.body.style.cursor = '';
-						  break;
+          if (e.pageY >= dialogPosition.top - resizeRegionSize && e.pageY <= dialogPosition.top + resizeRegionSize) {
+            tempResizeMode = 'n';
+          } else if (e.pageY >= dialogPosition.bottom - resizeRegionSize && e.pageY <= dialogPosition.bottom + resizeRegionSize) {
+            tempResizeMode = 's';
           }
-				}
-			}
+          if (e.pageX >= dialogPosition.left - resizeRegionSize && e.pageX <= dialogPosition.left + resizeRegionSize) {
+            tempResizeMode += 'w';
+          } else if (e.pageX >= dialogPosition.right - resizeRegionSize && e.pageX <= dialogPosition.right + resizeRegionSize) {
+            tempResizeMode += 'e';
+          }
 
-			function resizeEnd(e) {
+          resizeMode.value = tempResizeMode;
+
+          // Set cursor value according to resize mode
+          switch (tempResizeMode) {
+            case 'n':
+            case 's':
+              cursor.value = 'ns-resize';
+              document.body.style.cursor = 'ns-resize';
+              break;
+            case 'e':
+            case 'w':
+              cursor.value = 'ew-resize';
+              document.body.style.cursor = 'ew-resize';
+              break;
+            case 'ne':
+            case 'sw':
+              cursor.value = 'nesw-resize';
+              document.body.style.cursor = 'nesw-resize';
+              break;
+            case 'nw':
+            case 'se':
+              cursor.value = 'nwse-resize';
+              document.body.style.cursor = 'nwse-resize';
+              break;
+            default:
+              cursor.value = '';
+              document.body.style.cursor = '';
+              break;
+          }
+        }
+      }
+
+      function resizeEnd(e) {
         e.preventDefault();
 
-        if(isResize.value) {
+        if (isResize.value) {
           isResize.value = false;
           resizeMode.value = '';
           cursor.value = '';
-				}
-			}
+        }
+      }
 
       document.addEventListener('mousedown', resizeStart);
-			document.addEventListener('mousemove', drag);
       document.addEventListener('mousemove', resize);
-      document.addEventListener('mouseup', dragEnd);
       document.addEventListener('mouseup', resizeEnd);
 
       onBeforeUnmount(() => {
         document.removeEventListener('mousedown', resizeStart);
-        document.removeEventListener('mousemove', drag);
         document.removeEventListener('mousemove', resize);
-        document.removeEventListener('mouseup', dragEnd);
         document.removeEventListener('mouseup', resizeEnd);
-				if (isRender.value) detach(context.refs.dialog);
-			})
+      })
 
-			return {
+      return {
         isActive,
-				toggleDialog,
-				isMinimize,
-				toggleMinimize,
-				isMaximize,
-				toggleMaximize,
-				isRender,
-				dialogClasses,
-        dialogStyles,
-				dragStart
-			}
-		}
+        isRender,
+        toggleDialog,
+        isMinimize,
+        toggleMinimize,
+        isMaximize,
+        toggleMaximize,
+        wrapperClasses,
+        wrapperStyles,
+        dragStart
+      }
+    }
   }
 </script>
 <style scoped>
