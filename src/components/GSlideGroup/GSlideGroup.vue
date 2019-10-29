1130cb656e3d1528dc5601d51cc97979633b7aa0
longnh <hailong.nguyen95@gmail.com>
2019-10-29 16:50:29 +0700

    g-slide-group: refactoring

--- src/components/GSlideGroup/GSlideGroup.vue
+++ src/components/GSlideGroup/GSlideGroup.vue
@@ -1,23 +1,23 @@
 <template>
-	<div v-resize="setWidths" :class="classes" :style="styles">
-		<div :class="slideGroupPrevClasses" @click="onAffixClick('prev')">
-			<slot name="prev" @click="onAffixClick('prev')">
-				<i class="material-icons">{{prevIcon}}</i>
-			</slot>
-		</div>
+  <div v-resize="setWidths" :class="classes" :style="styles">
+    <div :class="slideGroupPrevClasses" @click="onAffixClick('prev')">
+      <slot name="prev" @click="onAffixClick('prev')">
+        <i class="material-icons">{{prevIcon}}</i>
+      </slot>
+    </div>
 
-		<div ref="wrapper" class="g-slide-group__wrapper">
-			<div ref="content" class="g-slide-group__content">
-				<slot name="content" :toggle="toggleItem" :active="isActiveItem"></slot>
-			</div>
+    <div ref="wrapper" class="g-slide-group__wrapper">
+      <div ref="content" class="g-slide-group__content">
+        <slot name="content" :toggle="toggleItem" :active="isActiveItem"></slot>
+      </div>
 
-		</div>
-		<div :class="slideGroupNextClasses" @click="onAffixClick('next')">
-			<slot name="next" @click="onAffixClick('next')">
-				<i class="material-icons">{{nextIcon}}</i>
-			</slot>
-		</div>
-	</div>
+    </div>
+    <div :class="slideGroupNextClasses" @click="onAffixClick('next')">
+      <slot name="next" @click="onAffixClick('next')">
+        <i class="material-icons">{{nextIcon}}</i>
+      </slot>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -53,11 +53,10 @@
     },
     directives: {
       Resize
-    }
-    ,
+    },
     setup(props, context) {
       //State date
-      let data = reactive({
+      const data = reactive({
         items: [],
         widths: {
           content: 0,
@@ -65,10 +64,10 @@
         }
       });
 
-      let isOverflowing = ref(false);
-      let internalItemsLength = ref(0);
-      let scrollOffset = ref(0);
-      let widths = computed(() => {
+      const isOverflowing = ref(false);
+      const internalItemsLength = ref(0);
+      const scrollOffset = ref(0);
+      const widths = computed(() => {
         return {
           content: 0,
           wrapper: 0
@@ -76,21 +75,21 @@
       });
 
       //Styles & classes computed
-      let slideGroupNextClasses = computed(() => {
+      const slideGroupNextClasses = computed(() => {
         return {
           'g-slide-group__next': true,
           'g-slide-group__prev--disabled': !hasNext.value,
         }
       });
 
-      let slideGroupPrevClasses = computed(() => {
+      const slideGroupPrevClasses = computed(() => {
         return {
           'g-slide-group__prev': true,
           'g-slide-group__prev--disabled': !hasPrev.value,
         }
       });
 
-      let classes = computed(() => {
+      const classes = computed(() => {
         return {
           'g-slide-group': true,
           'g-slide-group__has-affixes': hasAffixes.value,
@@ -98,7 +97,7 @@
         }
       });
 
-      let styles = computed(() => {
+      const styles = computed(() => {
 
       });
 
@@ -117,13 +116,13 @@
 
 
       //Has affixes when overflowing
-      let hasAffixes = computed(() => {
+      const hasAffixes = computed(() => {
         return (
           props.showArrows && isOverflowing.value
         )
       });
 
-      let hasNext = computed(() => {
+      const hasNext = computed(() => {
         if (!hasAffixes) {
           return false;
         }
@@ -134,7 +133,7 @@
       });
 
 
-      let hasPrev = computed(() => {
+      const hasPrev = computed(() => {
         return hasAffixes && scrollOffset.value !== 0
       });
 
@@ -161,19 +160,19 @@
           content: context.refs.content ? context.refs.content.clientWidth : 0,
           wrapper: context.refs.wrapper ? context.refs.wrapper.clientWidth : 0,
         }, scrollOffset.value);
-      };
+      }
 
       function calculateNewOffset(direction, widths, currentScrollOffset) {
         const newAbsoluteOffset = currentScrollOffset + (direction === 'prev' ? -1 : 1) * widths.wrapper;
 
         return Math.max(Math.min(newAbsoluteOffset, widths.content - widths.wrapper), 0);
-      };
+      }
 
       //onClick 'prev/next'
       function onAffixClick(location) {
         context.emit(`click:${location}`);
         scrollTo(location);
-      };
+      }
 
       function calculateUpdatedOffset(selectedElement, widths, currentScrollOffset) {
         const { offsetLeft, clientWidth } = selectedElement;
@@ -188,14 +187,14 @@
         }
 
         return currentScrollOffset
-      };
+      }
 
       function calculateCenteredOffset(selectedElement, widths) {
         const { offsetLeft, clientWidth } = selectedElement;
         const offsetCentered = offsetLeft + clientWidth / 2 - widths.wrapper / 2;
 
         return Math.min(widths.content - widths.wrapper, Math.max(0, offsetCentered));
-      };
+      }
 
       function scrollIntoView() {
         if (!selectedItem.value) {
@@ -230,14 +229,14 @@
           isOverflowing.value = data.widths.wrapper < data.widths.content;
           scrollIntoView();
         });
-      };
+      }
 
       //Model
-      let internalValue = computed({
+      const internalValue = computed({
         get: () => {
           if (props.value) {
             if (props.multiple && !Array.isArray(props.value)) {
-              props.value = [props.value];
+              return [props.value];
             }
             return props.value;
           }
@@ -248,27 +247,27 @@
         }
       });
 
-      let selectedIndex = computed(() => {
+      const selectedIndex = computed(() => {
         return internalValue.value ? props.items.findIndex((i) => i === internalValue.value) : -1;
       });
 
-      let selectedItem = computed(() => {
+      const selectedItem = computed(() => {
         return props.multiple ? undefined : selectedItems.value[0];
         //return context.refs.content.children[selectedIndex.value];
       });
 
-      let selectedItems = computed(() => {
+      const selectedItems = computed(() => {
         const clonedValue = _.clone(internalValue.value);
         const childrenArray = [...context.refs.content.children];
 
         let clonedInternalValue = !Array.isArray(clonedValue) ? [].concat(clonedValue) : clonedValue;
         let selectedValues = [];
         for (const value of clonedInternalValue) {
-          if(value) {
+          if (value) {
             selectedValues.push(_.findIndex(props.items, value));
           } else {
             selectedValues.push(-1);
-					}
+          }
         }
         return childrenArray.filter((item, index) => selectedValues.includes(index));
       });
