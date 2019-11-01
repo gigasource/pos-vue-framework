5a8f550c23a0d0cb921b6ee81d53d38434ea75ba
SonlamBKIC <phamsonlam1996@gmail.com>
2019-11-01 11:35:31 +0700

    Init V2

---
+++ src/components/GConnector/GConnector2.vue
@@ -0,0 +1,14 @@
+
+<script>
+  export default {
+    name: 'GConnector2',
+    props: {
+
+		},
+    setup (props, context) {
+
+		}
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/components/GConnector/GDiagram.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'GDiagram',
+    props: {
+
+		},
+    setup (props, context) {
+      function genSVG() {
+        return
+			}
+		}
+  }
+</script>
+<style scoped>
+</style>
