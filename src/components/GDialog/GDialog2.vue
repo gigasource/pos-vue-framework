75f10863456ee9c6ff384be33e0829f1fe544c95
SonlamBKIC <phamsonlam1996@gmail.com>
2019-11-14 10:41:28 +0700

    Init V2

---
+++ src/components/GDialog/GDialog2.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+<script>
+  export default {
+    name: 'GDialog2',
+    props: {},
+    data: function () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
