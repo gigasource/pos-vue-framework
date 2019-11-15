8ec33fbce75221ca1df6f9183fdc2533ad448506
longvatrong111 <longvatrong111@gmail.com>
2019-11-15 18:13:45 +0700

    template dashboard

--- src/POSView/Dashboard.vue
+++ src/POSView/Dashboard.vue
@@ -1,16 +1,59 @@
+<template>
+  <g-grid-layout style="height:100%" :layout="DashboardLayout">
+    <div area="header">
+      <g-toolbar class="header" gradient="indigo, blue">
+        <g-avatar size="37">
+          <g-img src="https://carlisletheacarlisletheatre.org/images/person-icon-flat-7.png"/>
+        </g-avatar>
+        <h6 class="user">Admin</h6>
+        <g-spacer/>
+        <div class="time">
+          <p>2:28 PM</p>
+          <p>14/11/2019</p>
+        </div>
+      </g-toolbar>
+    </div>
+    <div area="sidebar">
+      <g-sidebar class="sidebar">
+        <g-side-bar-tree-view :data="state.data" v-model="state.value"/>
+        <g-spacer/>
+        <div class="footer">
+          <g-icon style="margin: 16px 12px 13px 21px">{{LogOut}}</g-icon>
+          <span>Log Out</span>
+        </div>
+      </g-sidebar>
+    </div>
+    <div area="content">
+      <div area="welcome"><p class="welcome">Welcome !</p></div>
+      <div area="line">
+        <div class="line"/>
+      </div>
+      <div v-for="btn in btnData" :area="btn.area">
+        <g-btn class="btn" backgroundColor="#FFFFFF" height="100%">
+          <div>
+            <g-icon>{{btn.icon}}</g-icon>
+            <div class="btn-text">{{btn.text}}</div>
+          </div>
+        </g-btn>
+      </div>
+    </div>
+  </g-grid-layout>
+</template>
+
 <script>
-  import {reactive, computed} from "@vue/composition-api"
+  import {reactive, ref, computed} from "@vue/composition-api"
   //import component
   import GLayout from "../components/GLayout/GLayout";
   import GToolbar from "../components/GToolbar/GToolbar";
   import GSidebar from '../components/GSidebar/GSidebar';
   import GSideBarTreeView from "../components/GSidebar/GSideBarTreeView";
-  import GList from "../components/GList/GList";
   import GAvatar from "../components/GAvatar/GAvatar";
   import GIcon from "../components/GIcon/GIcon";
   import GImg from "../components/GImg/GImg";
   import GBtn from "../components/GBtn/GBtn";
   import GSpacer from "../components/GLayout/GSpacer";
+  import GGridLayout from "../components/GGridGenerator/GGridLayout";
+  import DashboardLayout from "./layout";
   //import icon
   import CashRegister from "../assets/dashboard/code.svg"
   import OrderHistory from "../assets/dashboard/history.svg"
@@ -22,32 +65,16 @@
   import LogOut from "../assets/dashboard/LogOut.svg"
 
   export default {
-    name: 'Dashboard',
-    components: {GLayout, GToolbar, GSidebar, GSideBarTreeView, GAvatar, GIcon, GBtn, GImg, GSpacer},
+    name: "DashboardTemplate",
+    components: {GGridLayout, GLayout, GToolbar, GSidebar, GSideBarTreeView, GAvatar, GIcon, GBtn, GImg, GSpacer},
     setup() {
-
-
-      function genHeader() {
-        return <g-toolbar class="header" gradient="indigo, blue">
-          <g-avatar size="37">
-            <g-img src="https://carlisletheacarlisletheatre.org/images/person-icon-flat-7.png"/>
-          </g-avatar>
-          <h6 class="user">Admin</h6>
-          <g-spacer/>
-          <div class="time">
-            <p>2:28 PM</p>
-            <p>14/11/2019</p>
-          </div>
-        </g-toolbar>
-      }
-
       const state = reactive({
         data: [{
           title: 'Dashboard', icon: 'mdi-cart-outline',
           items: [
             {title: 'Retail', icon: 'radio_button_unchecked', iconType: 'small'},
             {title: 'Retail', icon: 'radio_button_unchecked', iconType: 'small'},
-            {title: 'Retail', icon: 'radio_button_unchecked', iconType: 'small'}
+            {title: 'Retail', icon: 'radio_button_unchecked', iconType: 'small'},
           ]
         }, {
           title: 'Inventory', icon: 'mdi-cart-outline',
@@ -55,98 +82,37 @@
         value: null,
       })
 
-      function genSidebar() {
-
-        return <g-sidebar class="sidebar" width="100%">
-          <g-side-bar-tree-view data={state.data} vModel={state.value}/>
-          <g-spacer/>
-          <div class="footer">
-            <g-icon style="margin: 16px 12px 13px 21px">{LogOut}</g-icon>
-            <span>Log Out</span></div>
-        </g-sidebar>
-      }
-
-      function genContent() {
-        return <div class="content">
-          <p class="welcome">Welcome !</p>
-          <div class="line"/>
-          <g-btn class="btn1" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{CashRegister}</g-icon>
-              <div class="btn-text" style="margin-top: 15px">Cash Register</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn2" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{OrderHistory}</g-icon>
-              <div class="btn-text">Order History</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn3" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{StaffReport}</g-icon>
-              <div class="btn-text">Staff Report</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn4" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{Setting}</g-icon>
-              <div class="btn-text">Setting</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn5" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{AdminDashboard}</g-icon>
-              <div class="btn-text">Admin Dashboard</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn6" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{EndOfDayIcon}</g-icon>
-              <div class="btn-text">End of Day</div>
-            </div>
-          </g-btn>
-          <g-btn class="btn7" backgroundColor="#FFFFFF" height="100%">
-            <div>
-              <g-icon>{Support}</g-icon>
-              <div class="btn-text">Support</div>
-            </div>
-          </g-btn>
-        </div>
-      }
-
-      function genDashboard() {
-        return <g-layout class="container">
-          {genHeader()}
-          {genSidebar()}
-          {genContent()}
-        </g-layout>
-      }
+      const btnData = [
+        {area: 'btn1', text: 'Cash Register', icon: CashRegister,},
+        {area: 'btn2', text: 'Order History', icon: OrderHistory,},
+        {area: 'btn3', text: 'Staff Report', icon: StaffReport,},
+        {area: 'btn4', text: 'Setting', icon: Setting,},
+        {area: 'btn5', text: 'Admin Dashboard', icon: AdminDashboard,},
+        {area: 'btn6', text: 'End of Day', icon: EndOfDayIcon,},
+        {area: 'btn7', text: 'Support', icon: Support,}
+      ]
 
       return {
-        genDashboard
+        DashboardLayout,
+        //data
+        state,
+        btnData,
+        //icon
+        LogOut,
+        CashRegister,
+        OrderHistory,
+        StaffReport,
+        Setting,
+        AdminDashboard,
+        EndOfDayIcon,
+        Support,
       }
     },
-
-    render() {
-      return this.genDashboard()
-    }
   }
 </script>
 
 <style scoped lang="scss">
-  .container {
-    display: grid;
-    grid-template-columns: 23.75% 76.25%;
-    grid-template-rows: 8.25% 91.75%;
-    grid-template-areas: "header header" "sidebar content";
-    max-width: 100%;
-    max-height: 100%;
-    height: 100%;
-  }
-
   .header {
-    grid-area: header;
     //min-height: 40px;
     height: 100% !important;
 
@@ -182,7 +148,7 @@
   }
 
   .sidebar {
-    grid-area: sidebar;
+    height: 100%;
 
     &::v-deep .g-sidebar {
       position: relative;
@@ -218,68 +184,16 @@
       width: 243px;
       display: flex;
       align-items: center;
-      border-top: 2px solid #F2F2F2
+      border-top: 2px solid #F2F2F2;
     }
   }
 
   .content {
-    grid-area: content;
     background: #F2F2F2;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-    padding-left: 2.25%;
-    padding-right: 2.25%;
-
-    display: grid;
-    grid-template: [row1-start] "welcome welcome . ." 12% [row1-end] [row2-start] "btn1 btn2 btn3 ." 25% [row2-end] [row3-start] "line line line line" 12.5% [row3-end] [row4-start] "btn4 btn5 btn6 btn7" 25% [row4-end]
-      /23.875% 23.875% 23.875% 23.875%;
-    grid-column-gap: 1.5%;
-
-  }
-
-  .line {
-    grid-area: line;
-    align-self: center;
-    height: 1px;
-    background-color: #E0E0E0;
-  }
-
-  .btn1 {
-    grid-area: btn1;
-  }
-
-  .btn2 {
-    grid-area: btn2;
-  }
-
-  .btn3 {
-    grid-area: btn3;
-  }
-
-  .btn4 {
-    grid-area: btn4;
-  }
-
-  .btn5 {
-    grid-area: btn5;
-  }
-
-  .btn6 {
-    grid-area: btn6;
-  }
-
-  .btn7 {
-    grid-area: btn7;
-  }
-
-  .btn-text {
-    font-size: 16px;
-    letter-spacing: 0;
-    color: #424242;
-    margin-top: 20px;
   }
 
   .welcome {
-    grid-area: welcome;
     align-self: center;
     font-family: Muli;
     font-style: normal;
@@ -288,6 +202,23 @@
     line-height: 30px;
 
     color: #424242;
+  }
+
+  .line {
+    align-self: center;
+    height: 1px;
+    background-color: #E0E0E0;
+  }
+
+  .btn {
+    width: 100%;
+  }
+
+  .btn-text {
+    font-size: 16px;
+    letter-spacing: 0;
+    color: #424242;
+    margin-top: 20px;
   }
 
   h6 {
--- src/view/DashboardDemo.vue
+++ src/view/DashboardDemo.vue
@@ -1,40 +1,15 @@
 <template>
   <div style="height: 100%">
-    <g-btn @click="switchDashboard">Switch Dashboard</g-btn>
-    <dashboard :style="dashboardStyle"/>
-    <dashboard2 :style="dashboardStyle2"/>
+    <dashboard-template/>
   </div>
 </template>
 
 <script>
-  import Dashboard from "../POSView/Dashboard";
-  import Dashboard2 from "../POSView/Dashboard2";
-  import {reactive, computed} from "@vue/composition-api"
-  import GBtn from "../components/GBtn/GBtn";
+  import DashboardTemplate from "../POSView/Dashboard";
 
   export default {
     name: "DashboardDemo",
-    components: {GBtn, Dashboard, Dashboard2},
-    setup() {
-      const state = reactive({
-        display: true
-      })
-
-      function switchDashboard() {
-        state.display = !state.display
-      }
-
-      const dashboardStyle = computed(() => ({'display': state.display ? '' : 'none'}))
-
-      const dashboardStyle2 = computed(() => ({'display': state.display ? 'none' : ''}))
-
-
-      return {
-        switchDashboard,
-        dashboardStyle,
-        dashboardStyle2
-      }
-    }
+    components: {DashboardTemplate},
   }
 </script>
 
