87ce79d9b3a80036dda984261e7f5d78fce41355
longvatrong111 <longvatrong111@gmail.com>
2019-11-15 15:59:29 +0700

    remove unnecessary code

--- src/components/GIcon/__story__/GIcon.stories.js
+++ src/components/GIcon/__story__/GIcon.stories.js
@@ -77,7 +77,7 @@ export const iconInButton = () => ({
     left: {default: boolean('left', false)},
     right: {default: boolean('right', false)}
   },
-  template: `<div><g-btn>Buy this <b>{{color}}</b>   car    <g-icon :right="right" :left="left" :color="color" :dense="dense" :disabled="disabled" >fas fa-car</g-icon></g-btn></div>`,
+  template: `<div><g-btn>Buy-this-<b>{{color}}</b>-car<g-icon :right="right" :left="left" :color="color" :dense="dense" :disabled="disabled" >fas fa-car</g-icon></g-btn></div>`,
 })
 
 
