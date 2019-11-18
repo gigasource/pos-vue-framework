20da0887989e47c6a031540ca1dc644885ec70a0
chuoikhungbo <quanvantruong73@gmail.com>
2019-11-18 17:48:41 +0700

    add general setting

--- src/POSView/setting/view/viewGeneral.vue
+++ src/POSView/setting/view/viewGeneral.vue
@@ -3,36 +3,20 @@
 		<div style="padding-left: 256px">
 			<g-simple-table>
 				<tr>
-					<td>
-						Barcode
-					</td>
-					<td>
-						<g-switch color="#0091FF"/>
-					</td>
+					<td>Barcode</td>
+					<td><g-switch/></td>
 				</tr>
 				<tr>
-					<td>
-						Favorite article
-					</td>
-					<td>
-						<g-switch color="#0091FF"/>
-					</td>
+					<td>Favorite article</td>
+					<td><g-switch/></td>
 				</tr>
 				<tr>
-					<td>
-						Virtual keyboard
-					</td>
-					<td>
-						<g-switch color="#0091FF"/>
-					</td>
+					<td>Virtual keyboard</td>
+					<td><g-switch/></td>
 				</tr>
 				<tr>
-					<td>
-						Cashdrawer opens automatically
-					</td>
-					<td>
-						<g-switch color="#0091FF"/>
-					</td>
+					<td>Cashdrawer opens automatically</td>
+					<td><g-switch/></td>
 				</tr>
 			</g-simple-table>
 		</div>
@@ -53,6 +37,7 @@
   import GBtn from '../../../components/GBtn/GBtn';
   import GSimpleTable from '../../../components/GSimpleTable/GSimpleTable';
   import GSwitch from '../../../components/GSwitch/GSwitch';
+
   export default {
     name: 'viewGeneral',
     components: { GSwitch, GSimpleTable, GBtn, GIcon, GToolbar }
@@ -62,8 +47,15 @@
 <style scoped lang="scss">
 	.g-table {
 		::v-deep tr td {
-			width: 200px;
 			padding: 0 32px;
+		}
+
+		::v-deep tr td:first-child {
+			width: 200px;
+		}
+
+		::v-deep tr:not(:last-child) td {
+			border-bottom: 1px solid #D8D8D8;
 		}
 	}
 </style>
