445b4cb5b9b75fb9473cd4e327f6e2bcb910d0ed
Hoang Trong <longvatrong111@gmail.com>
2019-11-19 15:14:29 +0700

    merge master

--- src/POSComponents/POSInput/POSSelect.vue
+++ src/POSComponents/POSInput/POSSelect.vue
@@ -1,13 +1,57 @@
-<template>
-  
-</template>
-
 <script>
+    import GSelect from "../../components/GSelect/GSelect";
+
     export default {
-        name: "POSSelect"
+        name: "POSSelect",
+        mixins: [GSelect],
+        props: {
+
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .g-select::v-deep {
+    .g-menu--activator {
+      span {
+        margin: 3px
+      }
 
+      .g-tf-wrapper {
+        margin: 16px 0px 24px;
+      }
+
+      .g-tf-append__inner {
+        transition: transform 0.4s
+      }
+
+      .g-tf-input {
+        display: flex;
+      }
+
+      .input {
+        display: flex;
+      }
+
+      input {
+        flex-shrink: 1;
+        flex-grow: 1;
+        flex-basis: 0%;
+      }
+
+    }
+  }
+
+  .g-select__active::v-deep {
+    .g-tf-append__inner .g-icon:last-child {
+      transform: rotateZ(180deg);
+    }
+  }
+
+  /*.g-tf__filled {*/
+  /*  .g-tf-input::v-deep {*/
+  /*    box-shadow: none !important;*/
+  /*    padding: 11px 16px !important;*/
+  /*  }*/
+  /*}*/
 </style>
--- src/POSComponents/POSInput/__story__/POSInput.stories.js
+++ src/POSComponents/POSInput/__story__/POSInput.stories.js
@@ -2,6 +2,7 @@ import {text, withKnobs, number} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions'
 import PosTextField from "../POSTextField";
 import PosTextFieldLarge from "../POSTextFieldLarge";
+import PosSelect from "../POSSelect";
 import GIcon from "../../../components/GIcon/GIcon";
 
 export default {
@@ -48,5 +49,28 @@ export const larger = () => ({
                                    Append
                                  </template>
                                </pos-text-field-large>
+               </div>`
+})
+
+export const POSSelect = () => ({
+    components: {PosSelect, GIcon},
+    data() {
+        return {
+            items: [
+                {text: 'Jason Oner', value: 'https://cdn.vuetifyjs.com/images/lists/1.jpg'},
+                {text: 'Ranee Carlson', value: 'https://cdn.vuetifyjs.com/images/lists/2.jpg'},
+                {text: 'Cindy Baker', value: 'https://cdn.vuetifyjs.com/images/lists/3.jpg'},
+                {text: 'Ali Connors', value: 'https://cdn.vuetifyjs.com/images/lists/4.jpg'},
+            ],
+            selected: null
+        }
+    },
+    props: {},
+    template: `<div>
+                 <pos-select :items="items"
+                 item-Text="text"
+                 v-model="selected"
+                 >
+                 </pos-select>
                </div>`
 })
