2b1ae47b4cb869fb3871681bcaa940c945541fa4
Hoang Trong <longvatrong111@gmail.com>
2019-11-19 14:13:52 +0700

    Form text input

--- src/POSComponents/POSInput/__story__/POSInput.stories.js
+++ src/POSComponents/POSInput/__story__/POSInput.stories.js
@@ -1,6 +1,7 @@
 import {text, withKnobs, number} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions'
 import PosTextField from "../POSTextField";
+import PosTextFieldLarge from "../POSTextFieldLarge";
 import GIcon from "../../../components/GIcon/GIcon";
 
 export default {
@@ -18,8 +19,34 @@ export const POSTextField = () => ({
                                    Prepend
                                  </template>
                                  <template v-slot:prepend="{onClick}">
-                                   <g-icon>close</g-icon>
+                                   <g-icon color="red">mdi-pen</g-icon>
+                                 </template>
+                                 <template v-slot:append="{onClick}">
+                                   <g-icon color="red">mdi-pen</g-icon>
+                                 </template>
+                                 <template #apendContent>
+                                   Append
                                  </template>
                                </pos-text-field>
+               </div>`
+})
+
+export const larger = () => ({
+    components: {PosTextFieldLarge, GIcon},
+    props: {},
+    template: `<div><pos-text-field-large label="Name">
+                                 <template #prependContent>
+                                   Prepend
+                                 </template>
+                                 <template v-slot:prepend="{onClick}">
+                                   <g-icon color="red">mdi-pen</g-icon>
+                                 </template>
+                                 <template v-slot:append="{onClick}">
+                                   <g-icon>mdi-close</g-icon>
+                                 </template>
+                                 <template #apendContent>
+                                   Append
+                                 </template>
+                               </pos-text-field-large>
                </div>`
 })
