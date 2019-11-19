2e9edc7a0bd8105aab3962b79bb7dcbb15070d52
Pham Anh Tuan <tuanpa2295@gmail.com>
2019-11-19 18:22:50 +0700

    Fix article

--- src/POSView/article/Article.vue
+++ src/POSView/article/Article.vue
@@ -1,79 +1,79 @@
 <template>
   <g-grid-layout :layout="layout" style="height: 100%;">
-    <div area="category-title" style="padding: 8px; font-size: 13px"><p>Category </p></div>
-    <div area="category-content" style="padding: 5px;">
-      <p><b>{{productCategory}}</b></p>
+    <div area="category-title" class="title" style="padding: 16px 8px 8px 8px"><p>Category </p></div>
+    <div area="category-content" style="padding: 16px 8px 8px 8px">
+      <p style="font-size: 14px; font-weight: bold; line-height: 18px;">{{productCategory}}</p>
     </div>
-    <div area="product-title" style="padding: 8px; font-size: 13px"><p>Product </p></div>
-    <div area="product-content-left" style="padding: 5px;">
+    <div area="product-title" class="title" style="padding: 8px"><p>Product </p></div>
+    <div area="product-content-left" class="pa-2">
       <div>
-        <p>{{productName}}</p>
-        <p>{{productId}}</p>
+        <p style="font-size: 15px; line-height: 19px">{{productName}}</p>
+        <p class="product-info">{{productId}}</p>
       </div>
       <div style="display: block;">
         <p class="title">Unit </p>
-        <p>{{productUnit}}</p>
+        <p class="product-info">{{productUnit}}</p>
       </div>
       <div>
         <p class="title">Price </p>
-        <p>{{productPriceUnit}} {{productPrice}}</p>
+        <p class="product-info">{{productPriceUnit}} {{productPrice}}</p>
       </div>
     </div>
     <div area="product-content-right" style="padding: 5px;">
       <div>
         <p class="title">Barcode: </p>
-        <p>{{productBarcode}}</p>
+        <p class="product-info">{{productBarcode}}</p>
       </div>
       <div>
         <span class="title">Attribute:</span>
-        <div :key="index" v-for="(attribute, index) in productAttributes">
-          <span>{{attribute.attribute}}: </span>
-          <span>{{attribute.attributeDetail}}</span>
+        <div class="product-info" :key="index" v-for="(attribute, index) in productAttributes">
+          <span class="product-info">{{attribute.attribute}}: </span>
+          <span class="product-info">{{attribute.attributeDetail}}</span>
         </div>
       </div>
     </div>
-    <div area="color-title" style="padding: 8px; font-size: 13px"><p>Color</p></div>
-    <div area="color-content" style="padding: 5px;">
+    <div area="color-title" style="padding: 20px 8px 20px 8px" class="title"><p>Color</p></div>
+    <div area="color-content" style="padding: 5px 5px 5px 0;">
       <g-grid-select :grid="false" :items="buttonColors" v-model="selectedColor">
         <template #default="{toggleSelect, item, index}">
-          <g-btn :background-color="item.value" :key="index" :ripple="false" @click="toggleSelect(item)" style="margin: 12px; box-shadow: none; border-radius: 50%; width: 38px; min-width: 38px;height: 38px; border: 1px solid #D2D2D2;"></g-btn>
+          <g-btn :background-color="item.value" :key="index" :ripple="false" @click="toggleSelect(item)" style="margin-right: 17px; box-shadow: none; border-radius: 50%; width: 38px; min-width: 38px;height: 38px; border: 1px solid #D2D2D2;"></g-btn>
         </template>
         <template #selected="{toggleSelect, item, index}">
-          <g-badge overlay style="margin: 12px">
+          <g-badge overlay style="margin-right: 17px;">
             <template v-slot:badge>
-              <g-icon x-small>done</g-icon>
+              <g-icon>done</g-icon>
             </template>
-            <g-btn :background-color="item.value" :class="activeClass" @click="toggleSelect(item)" outlined style="box-shadow: none; border-radius: 50%; width: 38px; min-width: 38px; height: 38px;">
+            <g-btn :background-color="item.value" :class="activeClass" @click="toggleSelect(item)" style="box-shadow: none; border-radius: 50%; width: 38px; min-width: 38px; height: 38px;">
               {{item.optionTitle}}
             </g-btn>
           </g-badge>
         </template>
       </g-grid-select>
     </div>
-    <div area="sorting-title" style="padding: 8px; font-size: 13px"><p>Sorting</p></div>
+    <div area="sorting-title" class="title" style="padding: 8px"><p>Sorting</p></div>
     <div area="sorting-content" style="padding: 5px; display: flex; justify-content: center">
-      <div style="display: grid; grid-template-columns: 3fr; grid-template-rows: 3fr; grid-gap: 10px; width: 220px; height: 150px; padding: 10px">
-        <div style="grid-column: 2 / 3; grid-row: 1 / 2">
-          <g-btn outlined style="color: #979797">
-            <g-icon>mdi-menu-up</g-icon>
+      <div style="display: grid; grid-template-columns: 53px 62px 53px; grid-template-rows: 35px 46px 35px; grid-gap: 17px; width: 202px; height: 150px;">
+        <div style="grid-column: 2 / 3; grid-row: 1 / 2; width: 62px; height: 35px;">
+          <g-btn height="35" outlined style="color: #979797" width="62">
+            <g-icon size="34">mdi-menu-up</g-icon>
           </g-btn>
         </div>
-        <div style="grid-column: 2 / 3; grid-row: 3 / 4">
-          <g-btn outlined style="color: #979797">
-            <g-icon>mdi-menu-down</g-icon>
+        <div style="grid-column: 2 / 3; grid-row: 3 / 4;">
+          <g-btn height="35" outlined style="color: #979797" width="62">
+            <g-icon size="34">mdi-menu-down</g-icon>
           </g-btn>
         </div>
         <div style="grid-column: 2 / 3; grid-row: 2 / 3; display: flex; justify-content: center; align-items: center;">
-          <g-btn elevation="0" style="background-color: #EEEEEE; border: 3px solid #1271FF">5</g-btn>
+          <g-btn :height="46" :width="62" elevation="0" style="background-color: #EEEEEE; border: 3px solid #1271FF">5</g-btn>
         </div>
-        <div style="grid-column: 1 / 2; grid-row: 2 / 3">
-          <g-btn outlined style="color: #979797">
-            <g-icon>mdi-menu-left</g-icon>
+        <div style="grid-column: 1 / 2; grid-row: 2 / 3;">
+          <g-btn height="46" outlined style="color: #979797; min-width: 53px;" width="53">
+            <g-icon size="34">mdi-menu-left</g-icon>
           </g-btn>
         </div>
-        <div style="grid-column: 3 / 4; grid-row: 2 / 3">
-          <g-btn outlined style="color: #979797">
-            <g-icon>mdi-menu-right</g-icon>
+        <div style="grid-column: 3 / 4; grid-row: 2 / 3;">
+          <g-btn height="46" style="color: #979797;  min-width: 53px;" width="53" outlined>
+            <g-icon size="34">mdi-menu-right</g-icon>
           </g-btn>
         </div>
         <div></div>
@@ -98,7 +98,8 @@
     <div area="main">
       <g-scroll-window :show-arrows="false" area="window" v-model="window">
         <g-scroll-window-item :key="i" v-for="(items, i) in listItems">
-          <g-btn :background-color="item.color" :key="i" flat height="100%" v-for="(item, i) in items">{{item.title}}</g-btn>
+          <g-btn :background-color="item.color" :key="i" :style="item.style ? item.style : ''" flat height="100%" v-for="(item, i) in items">{{item.title}}
+          </g-btn>
         </g-scroll-window-item>
       </g-scroll-window>
 
@@ -134,9 +135,13 @@
       <g-btn height="100%" outlined>Discount</g-btn>
       <g-btn height="100%" outlined></g-btn>
       <g-btn height="100%" outlined>Plastic Refund</g-btn>
-      <g-btn area="btn-big" background-color="green lighten 1" height="100%" text text-color="white">Quick Cash</g-btn>
-      <g-btn background-color="orange lighten 1" height="100%" text text-color="white">Save</g-btn>
-      <g-btn background-color="blue darken 2" height="100%" text>
+      <g-btn area="btn-big" background-color="green lighten 1" height="100%" style="background: linear-gradient(41.19deg, #43A047 0%, #1DE9B6 100%);" text text-color="white">
+        Quick Cash
+      </g-btn>
+      <g-btn background-color="orange lighten 1" height="100%" style="background: linear-gradient(22.62deg, #FF6F00 0%, #FFCA28 100%);" text text-color="white">
+        Save
+      </g-btn>
+      <g-btn background-color="blue darken 2" height="100%" style="background: linear-gradient(22.62deg, #6200EA 0%, #1976D2 100%);" text>
         <router-link to="/payment">
           <span class="text-white">Pay</span>
         </router-link>
@@ -209,6 +214,11 @@
           text: 'red',
           value: 'red'
         },
+        {
+          id: 7,
+          text: 'brown',
+          value: 'brown'
+        },
       ],
       productColor: '',
       sortKey: '',
@@ -242,7 +252,7 @@
           { title: 'Tiramisu', color: 'teal accent 1' },
           { title: 'Tiramisu', color: 'blue lighten 3' },
           { title: 'Tiramisu', color: 'yellow accent 1' },
-          { title: 'Tiramisu', color: 'pink lighten 4' },
+          { title: 'Tiramisu', color: 'white', style: 'border: 3px solid #1271FF; ' },
           { title: 'Tiramisu', color: 'teal accent 1' },
           { title: 'Tiramisu', color: 'blue lighten 3' },
           { title: 'Tiramisu', color: 'yellow accent 1' },
@@ -341,9 +351,27 @@
 </script>
 
 <style lang="scss" scoped>
+  ::v-deep .g-badge {
+    background-color: #1271FF !important;
+    width: 12px;
+    height: 12px;
+    min-width: 12px;
+    left: 20px;
+    top: 5px;
+    .g-icon {
+      font-size: 10px !important;
+      font-weight: bold;
+    }
+  }
+  .title {
+    font-size: 13px;
+    font-weight: normal;
+    line-height: 16px;
+  }
 
-  .app .layout__right {
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);
+  .product-info {
+    font-size: 14px;
+    line-height: 18px;
   }
 
   .app {
@@ -441,6 +469,7 @@
       height: 100%;
     }
   }
+
 
   .g-btn {
     &.g-btn__outlined {
