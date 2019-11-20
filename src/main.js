063b1cfc5f4b8b3ee2306c6e7eac76c7b4c67be0
longnh <hailong.nguyen95@gmail.com>
2019-11-20 14:05:22 +0700

    change import path for main css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueCompositionApi from '@vue/composition-api';
 import router from './plugin/router';
 import VueRouter from 'vue-router';
 
+require('src/style/main.scss')
 require('./plugin/waves');
 
 Vue.config.productionTip = false;
