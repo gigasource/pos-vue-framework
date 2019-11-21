67e734d0355f529caa0dba99e8143691a761bef0
Hoang Trong <longvatrong111@gmail.com>
2019-11-21 14:01:50 +0700

    dashboard remake

--- src/POSView/Dashboard/Dashboard.vue
+++ src/POSView/Dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-	<g-grid-layout class="wrapper" :layout="DashboardLayout">
+	<g-grid-layout class="wrapper" :layout="Layout" :displayPreviewColor="false">
 		<div area="sidebar">
 			<g-sidebar>
         <template slot="header">
@@ -10,31 +10,30 @@
             <span>Admin</span>
           </div>
         </template>
-				<g-side-bar-tree-view :data="state.data" v-model="state.value"/>
+				<g-side-bar-tree-view :data="sidebarData.data" v-model="sidebarData.value"/>
 				<g-spacer/>
 				<div class="sidebar-footer waves-effect">
-          <g-icon>mdi-file</g-icon>
+          <g-icon svg>icon-logout</g-icon>
 					<span>Log Out</span>
 				</div>
 			</g-sidebar>
 		</div>
 		<div area="header" class="header">
-				<div class="time">
-					<p>2:28 PM</p>
-					<p>14/11/2019</p>
-				</div>
+					<div>2:28 PM</div>
+					<div>14/11/2019</div>
 		</div>
 		<div area="content">
-			<div area="welcome"><p class="welcome">Welcome !</p></div>
-			<div area="line">
-				<div class="line"/>
-			</div>
-			<div v-for="btn in btnData" :area="btn.area">
-				<g-btn class="btn" backgroundColor="#FFFFFF" height="100%">
-					<div>
-						<div class="btn-text">{{btn.text}}</div>
-					</div>
-				</g-btn>
+			<div area="welcome"><span class="welcome">Welcome !</span></div>
+			<div area="buttons">
+				<div area="line"><g-divider/></div>
+				<div v-for="btn in btnData" :area="btn.area">
+					<g-btn height="100%" width="100%" backgroundColor="#FFFFFF">
+						<div>
+							<g-icon size="60" svg>{{btn.icon}}</g-icon>
+							<div class="btn-text">{{btn.text}}</div>
+						</div>
+					</g-btn>
+				</div>
 			</div>
 		</div>
 	</g-grid-layout>
@@ -42,24 +41,22 @@
 
 <script>
     import {reactive} from "@vue/composition-api"
-    //import component
     import GGridLayout from "../../components/GGridGenerator/GGridLayout";
-    import DashboardLayout from "./layout";
-    import GToolbar from "../../components/GToolbar/GToolbar";
+    import Layout from "./layout";
     import GSidebar from '../../components/GSidebar/GSidebar';
     import GSideBarTreeView from "../../components/GSidebar/GSideBarTreeView";
     import GAvatar from "../../components/GAvatar/GAvatar";
-    import GIcon from "../../components/GIcon/GIcon";
     import GImg from "../../components/GImg/GImg";
     import GBtn from "../../components/GBtn/GBtn";
+    import GIcon from "../../components/GIcon/GIcon";
     import GSpacer from "../../components/GLayout/GSpacer";
-
+    import GDivider from "../../components/GLayout/GDivider";
 
     export default {
         name: "DashboardTemplate",
-        components: {GGridLayout, GToolbar, GSidebar, GSideBarTreeView, GAvatar, GIcon, GBtn, GImg, GSpacer},
+        components: {GGridLayout, GSidebar, GSideBarTreeView, GAvatar, GImg, GBtn, GIcon, GSpacer, GDivider},
         setup() {
-            const state = reactive({
+            const sidebarData = reactive({
                 data: [{
                     title: 'Dashboard', icon: 'mdi-cart-outline',
                     items: [
@@ -72,19 +69,18 @@
             })
 
             const btnData = [
-                {area: 'btn1', router: '/dashboard/cash-register', text: 'Cash Register'},
-                {area: 'btn2', router: '/dashboard/order-history', text: 'Order History'},
-                {area: 'btn3', router: '/dashboard/staff-report', text: 'Staff Report'},
-                {area: 'btn4', router: '/dashboard/setting', text: 'Setting'},
-                {area: 'btn5', router: '/dashboard/admin-dashboard', text: 'Admin Dashboard'},
-                {area: 'btn6', router: '/dashboard/end-of-day', text: 'End of Day'},
-                {area: 'btn7', router: '/dashboard/support', text: 'Support'}
+                {area: 'btn1', icon:'icon-code' , router: '/dashboard/cash-register', text: 'Cash Register'},
+                {area: 'btn2', icon:'icon-history' , router: '/dashboard/order-history', text: 'Order History'},
+                {area: 'btn3', icon:'icon-report' , router: '/dashboard/staff-report', text: 'Staff Report'},
+                {area: 'btn4', icon:'icon-gear' , router: '/dashboard/setting', text: 'Setting'},
+                {area: 'btn5', icon:'icon-dashboard' , router: '/dashboard/admin-dashboard', text: 'Admin Dashboard'},
+                {area: 'btn6', icon:'icon-calendar' , router: '/dashboard/end-of-day', text: 'End of Day'},
+                {area: 'btn7', icon:'icon-info' , router: '/dashboard/support', text: 'Support'}
             ]
 
             return {
-                DashboardLayout,
-                //data
-                state,
+                Layout,
+                sidebarData,
                 btnData,
             }
         },
