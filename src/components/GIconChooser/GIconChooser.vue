da828bd2ec8dcdb2afc10fbd2b84442f82690bbb
ThinhVu <vutrongthinhk7@gmail.com>
2019-11-25 16:24:49 +0700

    Reflex change whenever value in input changed

--- src/components/GIconChooser/GIconChooser.vue
+++ src/components/GIconChooser/GIconChooser.vue
@@ -295,7 +295,7 @@
       return () => {
         return <div class="g-icon-chooser">
           <div class="g-icon-chooser__activator">
-            <input class="g-icon-chooser__activator__input" value={state.value}/>
+            <input class="g-icon-chooser__activator__input" value={state.value} vOn:input={e => state.value = e.target.value}/>
             <g-icon class="g-icon-chooser__activator__preview" vOn:click={() => {
               state.showDialog = true
               initIconPickerDialogState()
@@ -338,6 +338,8 @@
 
       &__preview {
         padding: 0 5px;
+        min-width: 30px;
+        max-width: 30px;
 
         &:hover {
           background-color: #aaa;
