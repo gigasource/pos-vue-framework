2887624a761be2948ccf8505a1045b71d3761b84
danglam105 <danglam105@gmail.com>
2019-11-28 14:26:22 +0700

    Fix bug: missing some events in GBtn (#184)
    * Fix bug: missing some events in GBtn
    
    * Merge with update from master

--- src/components/GBtn/GBtn.vue
+++ src/components/GBtn/GBtn.vue
@@ -57,17 +57,23 @@
       const { classes, styles } = GBtnUtil(props, context);
 
       return { classes, styles }
-    }, render(h) {
+    },
+    render(h) {
       const genBtn = () => {
         const nodeData = {
           class: this.classes,
           style: this.styles,
-          directives: this.ripple ? [{ name: 'ripple', value: undefined }] : []
+          directives: this.ripple ? [{ name: 'ripple', value: undefined }] : [],
+          on: {
+            click: (e) => context.emit('click', e),
+            mouseenter: (e) => context.emit('mouseenter', e),
+            mouseleave: (e) => context.emit('mouseleave', e),
+            mouseup: (e) => context.emit('mouseup', e),
+            mousedown: (e) => context.emit('mousedown', e),
+          }
         };
 
-        return <button {...nodeData} vOn:click={(event) => {
-          this.$emit('click', event);
-        }}>
+        return <button {...nodeData}>
           <span className="g-btn__content">
             {this.$slots.default}
           </span>
