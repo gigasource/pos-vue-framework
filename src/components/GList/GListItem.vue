bb35ec70b1b3d09e1c8f033212472c0b51b5a2f2
Pay Duyen <duyenpay@gmail.com>
2019-11-29 17:58:39 +0700

    select list fixed

--- src/components/GList/GListItem.vue
+++ src/components/GList/GListItem.vue
@@ -1,60 +1,70 @@
 <template>
-	<div :class="classes" :style="styles" @click="onSelectItem" @keydown="onKeyDown">
+	<div :class="classes" :style="styles" tabindex="0" v-if='isItemAdded' v-on="listItemEvents(value, index)">
 		<slot></slot>
 	</div>
 </template>
 
 <script>
-  import { computed, ref } from '@vue/composition-api';
-	import {getInternalValue} from "../../utils/helpers";
+	import { computed, inject, ref } from '@vue/composition-api';
 
-  export default {
-    name: 'GListItem',
-    props: {
+	export default {
+		name: 'GListItem',
+		props: {
 			height: String,
 			disabled: Boolean,
-			selectable: Boolean,
-      twoLine: Boolean,
-      threeLine: Boolean,
-			isSelected: Boolean,
+			twoLine: Boolean,
+			threeLine: Boolean,
+			value: [String, Number, Object]
 		},
 		setup(props, context) {
-      const classes = computed(() => {
-        const defaultClasses = {
-          'g-list-item': true,
+			const classes = computed(() => {
+				const defaultClasses = {
+					'g-list-item': true,
 					'waves-effect': true
 				};
-        return {
-          ...defaultClasses,
+				return {
+					...defaultClasses,
 					'g-list-item__disabled': props.disabled,
-					'g-list-item__selectable': props.selectable,
-          'g-list-item__two-line': props.twoLine,
-          'g-list-item__three-line': props.threeLine,
-					'g-list-item__active': props.isSelected,
+					'g-list-item__two-line': props.twoLine,
+					'g-list-item__three-line': props.threeLine,
+					'g-list-item__active': isActiveItem(props.value),
 				}
 			});
-      const styles = computed(() => {
-        if(props.height) {
-          return {
-            'height': props.height
+			const styles = computed(() => {
+				if (props.height) {
+					return {
+						'height': props.height
 					}
 				}
 			});
-      const onSelectItem = () => {
-        context.emit('singleItemClick')
-			}
-			const onKeyDown = (e) => {
-      	context.emit('keydown', e)
-			}
 
-      return {
-        classes,
+			//handle listItem in List case
+			const add = inject('add')
+			const {isItemAdded, index} = add(props.value)
+			const isActiveItem = inject('isActiveItem')
+			const selectable = inject('selectable')
+			const inListEvents = inject('getListEvents')
+			const internalValue = ref(null)
+			const singleItemEvents = () => {
+				return {
+					click: () => {
+						context.emit('singleItemClick')
+					},
+				}
+			}
+			const listItemEvents = selectable ? inListEvents : singleItemEvents
+
+			return {
+				index,
+				classes,
 				styles,
-				onSelectItem,
-				onKeyDown,
+				listItemEvents,
+				internalValue,
+				isItemAdded,
+				singleItemEvents
 			}
 		}
-  }
+	}
 </script>
 
 <style scoped>
---
+++ src/components/GList/groupableForList.js
@@ -0,0 +1,164 @@
+import _ from 'lodash'
+import { computed, ref, watch } from '@vue/composition-api';
+
+export function groupableForList(props, vModel) {
+  //mandatory: requires at least 1 to be active at all times, unless value is null/undefined (at init)
+  //multiple: multiple items can be active at a time
+  //allowDuplicate: choose one item multiple times
+  const { returnObject = true, items, mandatory, multiple, allowDuplicates, maxSelection, itemValue, itemText } = props
+
+  const isObjectList = computed(() => items.some(item => _.isObject(item) === true))
+  const itemsHaveText = ref(items.filter(item => item[itemText]) || [])
+
+  //filter duplicate items for correct select effect
+  const uniqueItems = computed(() => {
+          if (items) {
+            if (isObjectList.value) {
+              if (returnObject) {
+                return _.uniqWith(itemsHaveText.value, _.isEqual)
+              } else if (itemValue) {
+                return _.uniqBy(itemsHaveText.value, itemText)
+              }
+              return _.uniqBy(itemsHaveText.value, itemText)
+            } else {
+              return _.uniq(items)
+            }
+          } else {
+            return []}}
+
+  )
+
+  const toggleItem = (item) => {
+    if (multiple) {
+      if (returnObject && itemText) {
+        updateMultiple(item);
+      } else {
+        if (itemValue ) updateMultiple(item[itemValue])
+        else if(!itemText) updateMultiple(item);
+        else updateMultiple(uniqueItems.value.indexOf(item))
+      }
+    } else {
+      if (returnObject ) {
+        updateSingle(item);
+      } else {
+        if(itemValue) {
+          updateSingle(item[itemValue])}
+        else if(!itemValue && !itemText) {
+          updateSingle(item);}
+        else updateSingle(uniqueItems.value.indexOf(item))
+      }
+    }
+  };
+
+  const updateSingle = (item) => {
+    const isSame = _.isEqual(vModel.value, item);
+    if (isSame && mandatory) {
+      return;
+    }
+    vModel.value = isSame ? null : item;
+  };
+
+  const updateMultiple = (item) => {
+    const clonedValue = _.clone(vModel.value);
+    const itemIndex = clonedValue.findIndex((i) => _.isEqual(i, item));
+    //item exists + mandatory
+    if (itemIndex > -1 && mandatory && clonedValue.length - 1 < 1) {
+      return;
+    }
+    if (itemIndex > -1 && !allowDuplicates) {
+      clonedValue.splice(itemIndex, 1);
+    } else {
+      clonedValue.push(item);
+    }
+    vModel.value = clonedValue;
+  };
+
+  const isActiveItem = (item) => {
+    if (multiple) {
+      if (returnObject) {
+        return vModel.value.some(element => _.isEqual(element, item))
+      } else {
+        return itemValue
+            ? vModel.value.includes(item[itemValue])
+            : (vModel.value.includes(uniqueItems.value.indexOf(item)) || vModel.value.includes(item))
+      }
+    }
+    if (returnObject) {
+      return _.isEqual(vModel.value, item)
+    } else {
+      return itemValue ? item[itemValue] === vModel.value : (uniqueItems.value.indexOf(item) === vModel.value || vModel.value === item)
+    }
+  };
+
+  return {
+    uniqueItems,
+    toggleItem,
+    isActiveItem
+  }
+}
+
+export function makeListSelectable(props, context) {
+
+  // 1 -> {a: 1, b: 2}
+  const convertValueToInternalValue = function (value) {
+
+    if (!props.itemValue || !value) {
+      return value;
+    }
+    if (!Array.isArray(props.value)) {
+      if (props.returnObject) {
+        return props.itemValue ? props.items.find(i => i[props.itemValue] === value): props.items.find(i => _.isEqual(i, value));
+      } else {
+        return props.items.find(i => i[props.itemValue] === value)[props.itemValue];
+      }
+    }
+    if (props.returnObject) {
+      return props.itemValue ? props.items.filter(i => value.some(el => el === i[props.itemValue])): null
+    } else if (props.itemValue) {
+      return value
+    }
+  }
+
+  // {a: 1, b: 2} -> 1
+  const convertInternalValueToValue = function (internalValue) {
+    if (!props.itemValue || !internalValue) {
+      return internalValue;
+    }
+    if (!Array.isArray(internalValue)) {
+      return internalValue
+    }
+    return internalValue.map(i => props.returnObject ? i : i);
+  }
+
+  let rawInternalValue;
+  if (props.multiple && props.value && !Array.isArray(props.value)) {
+    rawInternalValue = ref([convertValueToInternalValue(props.value)]);
+  } else if (props.multiple) {
+    rawInternalValue = ref(convertValueToInternalValue(props.value) || []);
+  } else {
+    rawInternalValue = ref(convertValueToInternalValue(props.value));
+  }
+
+
+  //use when props.value is change from outside
+  watch(() => props.value, () => {
+   if(_.isEqual(props.value, rawInternalValue.value)) return
+
+    if (props.multiple && props.value && !Array.isArray(props.value)) {
+      rawInternalValue.value = [convertValueToInternalValue(props.value)];
+    } else {
+      rawInternalValue.value = convertValueToInternalValue(props.value);
+    }
+  }, { lazy: true });
+
+  watch(() => rawInternalValue.value, () => {
+    if(_.isEqual(rawInternalValue.value, props.value)) return
+
+    context.emit('input', convertInternalValueToValue(rawInternalValue.value));
+  })
+
+  const { uniqueItems, toggleItem, isActiveItem } = groupableForList(props, rawInternalValue);
+
+  return { uniqueItems, toggleItem, isActiveItem, internalValue: rawInternalValue };
+}
+
