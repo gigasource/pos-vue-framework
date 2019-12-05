078f4941d5b56fba17b9e2facbf0c743fc6be01e
SonlamBKIC <phamsonlam1996@gmail.com>
2019-12-05 17:10:00 +0700

    Add story

--- src/components/GSnackbar/GSnackbar.vue
+++ src/components/GSnackbar/GSnackbar.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-  import getVModel from '../../mixins/getVModel';
+  import { getInternalValue } from '../../mixins/getVModel';
   import { setBackgroundColor } from '../../mixins/colorable';
   import { computed, watch } from '@vue/composition-api';
 
@@ -41,7 +41,7 @@
 			color: String,
 		},
 		setup(props, context) {
-      const { model: isActive } = getVModel(props, context);
+      const isActive = getInternalValue(props, context);
       let activeTimeout;
 
 			// Set close timeout
@@ -60,7 +60,7 @@
 			const snackClasses = computed(() => ({
         'g-snack__active': isActive.value,
 				'g-snack__absolute': props.absolute,
-        'g-snack__bottom': props.bottom || !props.top,
+        'g-snack__bottom': !props.top,
         'g-snack__left': props.left,
         'g-snack__right': props.right,
         'g-snack__top': props.top,
@@ -84,6 +84,106 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+	@import './_variables.scss';
+	@import '../../style/elevation.scss';
 
+	.g-snack {
+		align-items: center;
+		color: $snackbar-color;
+		display: flex;
+		font-size: 14px;
+		left: 8px;
+		pointer-events: none;
+		position: fixed;
+		right: 8px;
+		text-align: left;
+		z-index: 1000;
+		transition-duration: .15s;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+
+		&__absolute {
+			position: absolute
+		}
+
+		&__top {
+			top: 8px
+		}
+
+		&__bottom {
+			bottom: 8px
+		}
+
+		&-wrapper {
+			align-items: center;
+			background-color: $snackbar-background-color;
+			border-radius: 4px;
+			display: flex;
+			margin: 0 auto;
+			pointer-events: auto;
+			transition: inherit;
+			transition-property: opacity, transform;
+			min-width: 100%;
+			@include elevation(6)
+		}
+
+		&-content {
+			align-items: center;
+			display: flex;
+			min-height: 48px;
+			justify-content: space-between;
+			overflow: hidden;
+			padding: 8px 16px;
+			width: 100%;
+
+			::v-deep.g-btn {
+				color: $snackbar-color;
+				flex: 0 0 auto;
+				height: auto;
+				margin: 0 -8px 0 24px;
+				min-width: auto;
+				padding: 8px;
+				width: auto;
+
+				&::before {
+					display: none;
+				}
+			}
+		}
+	}
+
+	@media only screen and (min-width: 600px) {
+		.g-snack {
+			&-wrapper {
+				min-width: 344px;
+				max-width: 672px;
+
+				.g-snack__left & {
+					margin-left: 0;
+				}
+
+				.g-snack__right & {
+					margin-right: 0;
+				}
+			}
+
+			&__content .v-btn:first-of-type {
+				margin-left: 42px;
+			}
+		}
+	}
+
+	.g-snack-transition {
+		&-enter {
+			.g-snack-wrapper {
+				transform: scale(.8);
+			}
+		}
+
+		&-enter, &-leave-to {
+			.g-snack-wrapper {
+				opacity: 0;
+			}
+		}
+	}
 </style>
