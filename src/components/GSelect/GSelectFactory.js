f87ea4fbae53de22f7fb10bd75d42d42236bb39e
danglam105 <danglam105@gmail.com>
2019-12-20 15:10:54 +0700

    Add support for functional itemText & itemValue

--- src/components/GSelect/GSelectFactory.js
+++ src/components/GSelect/GSelectFactory.js
@@ -1,7 +1,23 @@
-import { computed } from '@vue/composition-api';
+import {computed} from '@vue/composition-api';
 import _ from 'lodash';
 
+function createItemFn(prop) {
+  return typeof prop === 'function'
+    ? prop
+    : item => {
+      if (!_.isObject(item)) return item
+
+      if (_.isArray(prop)) {
+        const key = prop.find(Object.keys(item).includes)
+        return item[key]
+      } else {
+        return item[prop]
+      }
+    }
+}
+
 const listMultipleFilter = (props, selectedValue) => {
+  const itemValueFn = computed(() => createItemFn(props.itemValue))
 
   let _options
   if (!selectedValue.value) {
@@ -13,16 +29,18 @@ const listMultipleFilter = (props, selectedValue) => {
     if (props.returnObject) _options = props.items.filter(item => !selectedValue.value.find(_selectedValue => _.isEqual(_selectedValue, item)));
     else {
       if (props.itemValue) {
-        _options = props.items.filter(item => !selectedValue.value.find(el => el === item[props.itemValue]))
+        _options = props.items.filter(item => !selectedValue.value.find(el => el === itemValueFn.value(item)))
       } else {
-        _options = props.items.filter(item => !(selectedValue.value.includes(item)) )
+        _options = props.items.filter(item => !(selectedValue.value.includes(item)))
       }
     }
-    }
-  return _options
   }
+  return _options
+}
 
 const searchTextFilteredItems = (props, state, items) => {
+  const itemTextFn = computed(() => createItemFn(props.itemText))
+
   if (!state.searchText || !state.searchText.trim()) {
     return items;
   }
@@ -35,17 +53,17 @@ const searchTextFilteredItems = (props, state, items) => {
   let _filteredOptions
   if (!props.filter) {
     let _searchedOptions = items.filter(item => {
-      const text = item[props.itemText] ? (item[props.itemText] + "").toLowerCase() : (item + "").toLowerCase();
+      const text = itemTextFn.value(item) ? (itemTextFn.value(item) + "").toLowerCase() : (item + "").toLowerCase();
       return text.startsWith(searchText);
     });
     _filteredOptions = _searchedOptions.concat(items.filter(item => {
-      const text = item[props.itemText] ? (item[props.itemText] + "").toLowerCase() : (item + "").toLowerCase();
+      const text = itemTextFn.value(item) ? (itemTextFn.value(item) + "").toLowerCase() : (item + "").toLowerCase();
       return !text.startsWith(searchText) && text.includes(searchText);
     }));
   }
   //use props.filter to filter items
   else if (typeof props.filter === 'function') {
-    _filteredOptions = items.filter(item => props.filter(item[props.itemText], searchText))
+    _filteredOptions = items.filter(item => props.filter(itemTextFn.value(item), searchText))
   }
   return _filteredOptions
 }
@@ -72,29 +90,34 @@ export function getList(props, selectedValue, state) {
 //selectedValue: object array --> return item match
 //then normalise item in form {text:, value:}
 export function getSelections(props, selectedValue) {
+  const itemTextFn = computed(() => createItemFn(props.itemText))
+  const itemValueFn = computed(() => createItemFn(props.itemValue))
+
   const isObjectList = props.items.some(item => _.isObject(item) === true)
   return computed(() => {
     if (!props.multiple) {
       let item = selectedValue.value;
       if (!item && item !== 0) return null;
-      if (!isObjectList ) return item;
-      if (props.itemValue && !props.returnObject) item = props.items.find(_item => _item[props.itemValue] === item);
-      else item = props.items.find(_item =>_.isEqual(item, _item))
-      return item ? {text: item[props.itemText], value: item[props.itemValue]} : '';
+      if (!isObjectList) return item;
+      if (props.itemValue && !props.returnObject) item = props.items.find(_item => itemValueFn.value(_item) === item);
+      else item = props.items.find(_item => _.isEqual(item, _item))
+      return item ? {text: itemTextFn.value(item), value: itemValueFn.value(item)} : '';
     }
     const list = selectedValue.value
-    if(!isObjectList) return list.map(item => props.items.find(el => el === item))
+    if (!isObjectList) return list.map(item => props.items.find(el => el === item))
     if (props.returnObject) {
-      if (props.itemValue) return list.map(item => ({ text: item[props.itemText], value: item[props.itemValue] }))
+      if (props.itemValue) return list.map(item => ({text: itemTextFn.value(item), value: itemValueFn.value(item)}))
       return list.map(item => props.items.find(el => _.isEqual(el, item)))
-    }
-    else if(props.itemValue) return list.map(item => props.items.find(el => el[props.itemValue] === item))
+    } else if (props.itemValue) return list.map(item => props.items.find(el => itemValueFn.value(el) === item))
     return list.map(item => props.items.find(el => _.isEqual(el, item)))
   })
 }
 
 //same as getSelection but accept selections not in list
 export function getSelectionsForCombobox(props, selectedValue) {
+  const itemTextFn = computed(() => createItemFn(props.itemText))
+  const itemValueFn = computed(() => createItemFn(props.itemValue))
+
   if (props.items === null || props.items.length === 0) {
     let empty = props.multiple ? [] : ''
     return selectedValue.value || empty
@@ -104,18 +127,20 @@ export function getSelectionsForCombobox(props, selectedValue) {
     let item = selectedValue.value;
     if (!item && item !== 0) return null
     if (!isObjectList) return item;
-    if (props.itemValue && !props.returnObject) {item = props.items.find(_item => _item[props.itemValue] === item) || item}
-    else item = props.items.find(_item => _.isEqual(item, _item)) || item
-    return item ? { text: item[props.itemText], value: item[props.itemValue] } : ''
-  }
-  else{
+    if (props.itemValue && !props.returnObject) {
+      item = props.items.find(_item => itemValueFn.value(_item) === item) || item
+    } else item = props.items.find(_item => _.isEqual(item, _item)) || item
+    return item ? {text: itemTextFn.value(item), value: itemValueFn.value(item)} : ''
+  } else {
     const list = selectedValue.value || []
     if (!isObjectList) return list
     if (props.returnObject) {
-      if (props.itemValue) return list.map(item => item[props.itemValue] ? { text: item[props.itemText], value: item[props.itemValue] } : item)
+      if (props.itemValue) return list.map(item => itemValueFn.value(item) ? {
+        text: itemTextFn.value(item),
+        value: itemValueFn.value(item)
+      } : item)
       return list
-    }
-    else if (props.itemValue) return list.map(item => props.items.find(el => el[props.itemValue] === item) || item)
+    } else if (props.itemValue) return list.map(item => props.items.find(el => itemValueFn.value(el) === item) || item)
     return list
   }
 
