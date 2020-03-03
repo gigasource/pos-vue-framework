470985ca2bb894832d442442521dcee5978aaba5
longnh301 <43654367+longnh301@users.noreply.github.com>
2020-03-03 10:57:34 +0700

    Revert "Fixed select box arrow (#340)"
    This reverts commit b5b1cbc0dd46f8b95f718c85187590b4876e21b4.

--- src/components/GCombobox/selectComponentHOC.js
+++ src/components/GCombobox/selectComponentHOC.js
@@ -242,16 +242,11 @@ const componentsFactory = (component, componentName) => {
 
       }
       const textFieldScopedSlots = {
-        ...context.slots['append-inner']
-          ? {
+        ...context.slots['append-inner'] && {
           'append-inner': ({ iconColor }) =>
             [<GIcon color={iconColor} class={['g-icon__arrow']}>arrow_drop_down</GIcon>,
               context.slots['append-inner'] && context.slots['append-inner']()]
-          }
-          : {
-            'append-inner': ({ iconColor }) =>
-              <GIcon color={iconColor} class={['g-icon__arrow']}>arrow_drop_down</GIcon>
-          },
+        },
         ...context.slots['append-outer'] && { 'append-outer': () => context.slots['append-outer']() },
         'input-slot': () =>
           <template style={{ display: 'flex' }}>
