0430aad6b760342da2309f348ddf2ceb97a55104
danglam105 <danglam105@gmail.com>
2020-03-24 16:17:31 +0700

    Update drag & drop directive to include File object + export directives (#380)
    * Update drag & drop directive to include File object + export directives
    
    * Add missing file from previous commit

---
+++ src/directives/index.js
@@ -0,0 +1,19 @@
+import ClickOutside from './click-outside/click-outside'
+import Draggable from './drag-and-drop/draggable'
+import Droppable from './drag-and-drop/droppable'
+import Intersect from './intersect/intersect'
+import Resize from './resize/resize'
+import Ripple from './ripple/ripple'
+import Touch from './touch/touch'
+import Zoom from './zoom/zoom'
+
+export {
+  ClickOutside,
+  Draggable,
+  Droppable,
+  Intersect,
+  Resize,
+  Ripple,
+  Touch,
+  Zoom,
+}
