84a37aad51bebfbe5ed87c485aa18567895bbc7a
Thinh Vu <vutrongthinhk7@gmail.com>
2020-04-10 12:16:19 +0700

    Add g edit view input component (#387)
    * Add GEditViewInput component
    
    * Remove console log.
    Add component to index

---
+++ src/components/GEditViewInput/GEditViewInput.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="g-edit-view-input">
+    <input v-if="mode === 'edit'"
+           :key="mode"
+           :value="internalValue"
+           :style="style"
+           :class="inputClasses"
+           ref="input"
+           @input="oninput"
+           @keydown.enter="applyChange"/>
+    <div ref="span" v-if="mode !== 'edit'" class="g-edit-view-input__span">{{ internalValue }}</div>
+    <g-icon v-if="!readonly && mode !== 'edit'" @click="switchToEditMode">edit</g-icon>
+    <g-icon v-if="!readonly && mode === 'edit'" @click="applyChange">fas fa-check</g-icon>
+    <g-icon v-if="!readonly && mode === 'edit'" @click="resetValue">mdi-close</g-icon>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GEditViewInput',
+    props: {
+      value: { type: String, default: '' },
+      readonly: Boolean,
+    },
+    data() {
+      return {
+        // In EVI view mode, input isn't show, so calling focus, select from input element will not work (because input is not display)
+        // to know whether user just switch from view -> edit mode, we need to store EVI previous view mode
+        // when VDOM updated, we will check if EVI just switch from view -> edit mode, then we will focus to input and highlight it's content
+        previousMode: 'view',
+        mode: 'view',
+        style: {
+          'user-select': 'text !important',
+          '-webkit-user-select': 'text !important',
+        },
+        internalValue: this.value || ''
+      }
+    },
+    computed: {
+      inputClasses() {
+        return {
+          'g-edit-view-input__input': true,
+          'g-edit-view-input__input--edit': this.mode === 'edit',
+        }
+      }
+    },
+    watch: {
+      value(val) {
+        this.internalValue = val
+      }
+    },
+    updated() {
+      // check if EVI just switch from view -> edit mode then select all input content
+      if (this.mode === 'edit' && this.previousMode === 'view') {
+        this.$refs.input.focus()
+        this.$refs.input.select()
+      }
+    },
+    methods: {
+      switchToEditMode() {
+        this.previousMode = this.mode
+        this.mode = 'edit'
+      },
+      oninput(e) {
+        if (this.previousMode !== this.mode)
+          this.previousMode = this.mode
+        this.internalValue = e.target.value
+      },
+      applyChange() {
+        this.mode = 'view'
+        this.$emit('input', this.internalValue, changeApplied => {
+          if (!changeApplied)
+            this.internalValue = this.value
+        })
+      },
+      resetValue() {
+        this.mode = 'view'
+        this.internalValue = this.value
+        this.$emit('input', this.value)
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .g-edit-view-input {
+    position: relative;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+
+    &__input {
+      outline: none;
+      background-color: transparent;
+      border-bottom: 2px solid transparent;
+      font-weight: inherit;
+      font-size: inherit;
+      font-family: inherit;
+      width: 100%;
+
+      &--edit {
+        border-bottom: 2px solid #536DFE;
+        pointer-events: initial;
+      }
+    }
+
+    &__span {
+      cursor: default;
+      word-break: break-all;
+      -webkit-line-clamp: 1;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+  }
+
+  .edit-btn {
+    width: 14px;
+    height: 14px;
+    cursor: pointer;
+    margin-left: 4px;
+  }
+</style>
